66e2810b8d8fd0d8b0bed5590a31324ec1d2e5f8
xing wang <uwspstar@gmail.com>
2019-09-01 07:05:06 -0500

    no message

---
+++ Nodejs-Projects/play-ground/first-app/app.js
@@ -0,0 +1,14 @@
+console.log(__filename);
+console.log(__dirname);
+
+(function sayHello() {
+    console.log("hello")
+})();
+
+
+const sayHi = function () {
+    console.log("hi")
+
+}
+
+sayHi();
