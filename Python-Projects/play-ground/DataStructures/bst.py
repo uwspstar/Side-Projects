23deae522146b2e8191131208a4545c20376934b
xing wang <uwspstar@gmail.com>
2019-09-07 09:41:46 -0500

    no message

---
+++ Python-Projects/play-ground/DataStructures/bst.py
@@ -0,0 +1,8 @@
+# Binary Search Trees
+# 1) has to be sored
+# 2) fast for larger set
+# 3) each node is greater than every node in its left subtree
+
+# BST insert
+# 1) start at root
+# 2) always insert as a leaf
