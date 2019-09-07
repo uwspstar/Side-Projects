71d690e6fdbefba16f9a3cb79ecf14d65e846ecb
xing wang <uwspstar@gmail.com>
2019-09-07 09:29:47 -0500

    no message

---
+++ Python-Projects/play-ground/DataStructures/list.py
@@ -0,0 +1,9 @@
+letters = ["a", "b", "c"]
+matrix = [[0, 1], [2, 3]]
+zeros = [0] * 5
+combined = zeros + letters
+numbers = list(range(10))
+chars = list("hello World")
+print(chars)
+print(numbers)
+print(combined)
