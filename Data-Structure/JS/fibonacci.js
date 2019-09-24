af4437091be05a29c608c634d6b50f4b011f1ad9
xing wang <uwspstar@gmail.com>
2019-09-24 05:49:05 -0500

    no message

---
+++ Data-Structure/JS/fibonacci.js
@@ -0,0 +1,12 @@
+// const fibonacciRecursive = function() {
+//     let result =
+// }
+
+const fibonacci = function (n) {
+    if (n <= 2) return 1;
+    let result = [1, 1];
+    for (let i = 2; i < n; i++) {
+        result[i] = result[i - 1] + result[i - 2];
+    }
+    return result[n - 1];
+}
