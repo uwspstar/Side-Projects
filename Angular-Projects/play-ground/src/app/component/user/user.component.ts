cb1c9d64c65b94bbc789189c6a99d23d34b90e56
xing wang <uwspstar@gmail.com>
2019-09-25 16:00:46 -0500

    no message

--- Angular-Projects/play-ground/src/app/component/user/user.component.ts
+++ Angular-Projects/play-ground/src/app/component/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-user',
@@ -6,11 +7,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
+  users: User[];
   constructor() { }
 
   ngOnInit() {
     // when componet initialize
+    console.log('init....');
+
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 30,
+        address: {
+          street: '100 ABC',
+          city: 'Austin',
+          state: 'TX'
+        }
+      },
+      {
+        firstName: 'Lin',
+        lastName: 'Doe',
+        age: 30,
+        address: {
+          street: '15100 ABC',
+          city: 'Dollas',
+          state: 'TX'
+        }
+      }
+    ];
+
+    this.addUser({
+      firstName: 'Xw',
+      lastName: 'Wee',
+      age: 20,
+      address: {
+        street: '100 ABC',
+        city: 'Austin',
+        state: 'TX'
+      }
+    });
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
   }
 
 }
