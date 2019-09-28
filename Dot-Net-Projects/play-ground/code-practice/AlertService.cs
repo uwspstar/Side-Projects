35f2fa8fccf8f945a21dde7da68f37b8d9613fb4
xing wang <uwspstar@gmail.com>
2019-09-28 07:52:13 -0500

    Create AlertService.cs

---
+++ Dot-Net-Projects/play-ground/code-practice/AlertService.cs
@@ -0,0 +1,52 @@
+// Refactor the AlertService and AlertDAO classes:
+
+// Create a new interface, named IAlertDAO, that contains the same methods as AlertDAO.
+// AlertDAO should implement the IAlertDAO interface.
+// AlertService should have a constructor that accepts IAlertDAO.
+// The RaiseAlert and GetAlertTime methods should use the object passed through the constructor.
+
+using System.Collections.Generic;
+using System;
+
+public interface IAlertDAO
+{
+    Guid AddAlert(DateTime time);
+    DateTime GetAlert(Guid id);
+}
+
+public class AlertService	
+{
+    private readonly IAlertDAO storage = new AlertDAO();
+    
+    public AlertService(IAlertDAO storage) { 
+        
+        this.storage = storage;
+    }
+	
+    public Guid RaiseAlert()
+    {
+        return this.storage.AddAlert(DateTime.Now);
+    }
+	
+    public DateTime GetAlertTime(Guid id)
+    {
+        return this.storage.GetAlert(id);
+    }	
+}
+	
+public class AlertDAO : IAlertDAO
+{
+    private readonly Dictionary<Guid, DateTime> alerts = new Dictionary<Guid, DateTime>();
+	
+    public Guid AddAlert(DateTime time)
+    {
+        Guid id = Guid.NewGuid();
+        this.alerts.Add(id, time);
+        return id;
+    }
+	
+    public DateTime GetAlert(Guid id)
+    {
+        return this.alerts[id];
+    }	
+}
