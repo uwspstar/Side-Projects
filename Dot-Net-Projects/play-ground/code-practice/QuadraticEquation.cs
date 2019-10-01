fb62f5abd28369226bb5e5cdeb045ff36ed7618b
xing wang <uwspstar@gmail.com>
2019-10-01 08:20:31 -0500

    Update QuadraticEquation.cs

--- Dot-Net-Projects/play-ground/code-practice/QuadraticEquation.cs
+++ Dot-Net-Projects/play-ground/code-practice/QuadraticEquation.cs
@@ -32,3 +32,26 @@ public class QuadraticEquation
         Console.WriteLine("Roots: " + roots.Item1 + ", " + roots.Item2);
     }
 }
+
+/*
+using System;
+
+public class QuadraticEquation
+{
+    public static Tuple<double, double> FindRoots(double a, double b, double c)
+    {
+        var m = (b*b) - (4 * a * c);
+        m = Math.Sqrt(m);
+        var x1 = (-1.0 * b + m ) / (2*a);
+        var x2 = (-1.0 * b - m ) / (2*a);
+            
+        return Tuple.Create(x1, x2);
+    }
+
+    public static void Main(string[] args)
+    {
+        Tuple<double, double> roots = QuadraticEquation.FindRoots(2, 10, 8);
+        Console.WriteLine("Roots: " + roots.Item1 + ", " + roots.Item2);
+    }
+}
+*/
