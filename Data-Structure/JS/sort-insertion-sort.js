f94b062f56f027d5a1ebe2f09d48949d6bb57b51
xing wang <uwspstar@gmail.com>
2020-02-17 06:00:11 -0600

    a lot of changes, un track  machine learning project

--- Data-Structure/JS/sort-insertion-sort.js
+++ Data-Structure/JS/sort-insertion-sort.js
@@ -1,18 +1,26 @@
 const arr = [0, 1, 5, 4, 3, 2, 6];
 
-const insertionSort = function (arr) {
-    if (arr.length < 2) return arr;
+/*
 
-    for (let i = 0; i < arr.length; i++) {
-        let smallIdx = i;
-        for (let j = i; j < arr.length; j++) {
-            if (arr[smallIdx] > arr[j]) {
-                smallIdx = j;
-            }
+Insertion Sort Pseudocode
+
+Start by picking the second element in the array
+Now compare the second element with the one before it and swap if necessary.
+Continue to the next element and if it is in the incorrect order, iterate through the sorted portion (i.e. the left side) to place the element in the correct place.
+Repeat until the array is sorted
+
+*/
+const insertSort = function (arr) {
+    let i, j;
+    for (i = 1; i < arr.length; i++) {
+        let current = arr[i];
+        for (j = i - 1; j >= 0; j--) {
+            if (current < arr[j]) {
+                arr[j + 1] = arr[j];
+            } else break;
         }
-        [arr[i], arr[smallIdx]] = [arr[smallIdx], arr[i]];
+        arr[j + 1] = current;
     }
     return arr;
 }
-
 console.log(insertionSort(arr));
