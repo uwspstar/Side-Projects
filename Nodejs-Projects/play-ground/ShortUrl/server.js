8de4e5b91db590b9a4087594550581e2c4953164
xing wang <uwspstar@gmail.com>
2020-03-29 14:11:08 -0500

    update

--- Nodejs-Projects/play-ground/ShortUrl/server.js
+++ Nodejs-Projects/play-ground/ShortUrl/server.js
@@ -1,15 +1,47 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
 const app = express()
+
+
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+app.use(express.urlencoded({
+    extended: false
+}))
 
 app.set('view engine', 'ejs')
 
-app.post('shortUrls', (req, res) => {
-    res.render('index')
+app.get('/', async (req, res) => {
+    const shortUrls = await ShortUrl.find()
+    res.render('index', {
+        shortUrls: shortUrls
+    })
 })
-app.get('/', (req, res) => {
-    res.render('index')
-});
+
+app.post('/shortUrls', async (req, res) => {
+    await ShortUrl.create({
+        full: req.body.fullUrl
+    })
+
+    res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req, res) => {
+    const shortUrl = await ShortUrl.findOne({
+        short: req.params.shortUrl
+    })
+    if (shortUrl == null) return res.sendStatus(404)
+
+    shortUrl.clicks++
+    shortUrl.save()
+
+    res.redirect(shortUrl.full)
+})
 
 const port = process.env.PORT || 3000
 
