52f5b33e88b44cf7f18331d237aa93e2677aaad1
xing wang <uwspstar@gmail.com>
2020-03-28 19:02:50 -0500

    middleware

--- Nodejs-Projects/play-ground/Middleware/server.js
+++ Nodejs-Projects/play-ground/Middleware/server.js
@@ -4,24 +4,39 @@ const app = express();
 // globle middleware
 app.use(loggingMiddleware);
 
-app.get('/', (req, res) => {
+app.get('/', middleware, (req, res) => {
+    console.log('Home Page ...')
     res.send('Home Page')
 })
 
 app.get('/users', authMiddleware, (req, res) => {
+    console.log(req.admin)
     res.send('Users Page')
 })
 
 function loggingMiddleware(req, res, next) {
     console.log('Inside Middleware')
     console.log(`${new Date().toISOString()}: ${req.originalUrl}`)
-
     next()
 }
 
-function authMiddleware(req, res, next) {
-    console.log('authMiddleware')
+function middleware(req, res, next) {
+    console.log('Before Next')
     next()
+    console.log('After Next')
+}
+
+function authMiddleware(req, res, next) {
+
+    console.log('authMiddleware')
+    if (req.query.admin === 'true') {
+        req.admin = true
+        next()
+    } else {
+        res.send('ERROR: You must be an admin')
+    }
+
+
 }
 
 app.listen(3000, () => console.log(`listening on 3000`))
