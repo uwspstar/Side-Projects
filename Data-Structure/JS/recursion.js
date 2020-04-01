78d3037ac33ec997e0da2ce3517541db687d3137
xing wang <uwspstar@gmail.com>
2020-03-31 20:29:31 -0500

    recursions

--- Data-Structure/JS/recursion.js
+++ Data-Structure/JS/recursion.js
@@ -1,1 +1,9 @@
 // https://www.udemy.com/course/coding-interview-bootcamp-algorithms-and-data-structure/learn/lecture/8547214#overview
+
+function factorial(n) {
+    if (n == 1) return 1;
+    return n * factorial(n - 1);
+}
+
+console.log('factorial(5) = ' + factorial(5));
+console.log('factorial(3) = ' + factorial(3));
