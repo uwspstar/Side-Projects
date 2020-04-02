0f6d1e37482fa54b722ebcc8a3ceebe4bd90eefc
xing wang <uwspstar@gmail.com>
2020-04-01 21:08:12 -0500

    sort

--- Data-Structure/JS/searching/naiveSearch.js
+++ Data-Structure/JS/searching/naiveSearch.js
@@ -1,3 +1,4 @@
+/*
 function naiveSearch(long, short) {
     var count = 0;
     for (var i = 0; i < long.length; i++) {
@@ -10,3 +11,15 @@ function naiveSearch(long, short) {
 }
 
 naiveSearch("lorie loled", "lol")
+*/
+
+const searchMathStr = function (long, short) {
+    let count = 0;
+    for (let i = 0; i < long.length; i++) {
+        for (let j = 0; j < short.length; j++) {
+            if (short[j] !== long[i + j]) break;
+            if (j === short.length - 1) count++;
+        }
+    }
+    return count;
+}
