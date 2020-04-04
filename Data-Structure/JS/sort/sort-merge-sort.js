50a1de728dd2ef0849faeac00fd7589725ecf221
xing wang <uwspstar@gmail.com>
2020-04-04 09:29:11 -0500

    merge sort

--- Data-Structure/JS/sort/sort-merge-sort.js
+++ Data-Structure/JS/sort/sort-merge-sort.js
@@ -1,7 +1,15 @@
 /*
+https://visualgo.net/en/sorting?slide=1
+
 merge sort:
 It's a combination of two things - merging and sorting
 
 Try out bubble sort on an array of 100000 elements, it will take quite some time!
 There is a family of sorting algorithms that can improve time complexity from O(n  ) to O(n log n)
+
+In order to implement merge sort, it's useful to first implement a function responsible for merging two sorted arrays
+
+Given two arrays which are sorted, this helper function should create a new array which is also sorted, and consists of all of the elements in the two input arrays
+
+This function should run in O(n + m) time and O(n + m) space and should not modify the parameters passed to it.
 */
