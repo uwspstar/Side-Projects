4822226eea4e8ff3f2fad44fce2c99d538bdc85a
xing wang <uwspstar@gmail.com>
2020-04-04 17:26:12 -0500

    selection sort

--- Data-Structure/JS/sort/sort-insertion-sort.js
+++ Data-Structure/JS/sort/sort-insertion-sort.js
@@ -1,13 +1,15 @@
 const arr = [0, 1, 5, 4, 3, 2, 6];
 
 /*
+Insertion Sort
+Builds up the sort by gradually creating a larger left half which is always sorted
 
 Insertion Sort Pseudocode
 
-Start by picking the second element in the array
+- Start by picking the second element in the array
 Now compare the second element with the one before it and swap if necessary.
-Continue to the next element and if it is in the incorrect order, iterate through the sorted portion (i.e. the left side) to place the element in the correct place.
-Repeat until the array is sorted
+- Continue to the next element and if it is in the incorrect order, iterate through the sorted portion (i.e. the left side) to place the element in the correct place.
+- Repeat until the array is sorted
 
 */
 const insertSort = function (arr) {
