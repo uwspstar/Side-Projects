d0e387279d4d6a127fa462b0c6e63373b8de5ca9
xing wang <uwspstar@gmail.com>
2020-04-04 17:07:32 -0500

    bubble sort

--- Data-Structure/JS/sort/sort-bubble-sort.js
+++ Data-Structure/JS/sort/sort-bubble-sort.js
@@ -1,18 +1,28 @@
+/*
+BubbleSort Pseudocode
+Let's visualize this!
+
+Start looping from with a variable called i the end of the array towards the beginning
+Start an inner loop with a variable called j from the beginning until i - 1
+If arr[j] is greater than arr[j+1], swap those two values!
+Return the sorted array
+*/
+
 const arr = [0, 1, 5, 4, 3, 2, 6];
 
 const bubbleSort = function (arr) {
 
-    if (arr.length < 2) return arr;
+  if (arr.length < 2) return arr;
 
-    for (let i = arr.length - 1; i >= 0; i--) {
-        for (let j = 0; j <= i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-            }
-        }
-        console.log(arr, i);
+  for (let i = arr.length - 1; i >= 0; i--) {
+    for (let j = 0; j <= i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
     }
-    return arr;
+    console.log(arr, i);
+  }
+  return arr;
 }
 
 console.log(bubbleSort(arr));
