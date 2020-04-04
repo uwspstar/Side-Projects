b18450d85f76eaaf33093a26be7fe475a7f46eca
xing wang <uwspstar@gmail.com>
2020-04-04 09:22:08 -0500

    merge sort

---
+++ Data-Structure/JS/sort/sort-merge-sort.js
@@ -0,0 +1,7 @@
+/*
+merge sort:
+It's a combination of two things - merging and sorting
+
+Try out bubble sort on an array of 100000 elements, it will take quite some time!
+There is a family of sorting algorithms that can improve time complexity from O(n  ) to O(n log n)
+*/
