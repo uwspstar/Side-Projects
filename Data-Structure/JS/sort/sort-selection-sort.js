5a0f2d7f924f1a4e21612040c663d131a31798f0
xing wang <uwspstar@gmail.com>
2020-04-04 17:22:12 -0500

    selection sort

--- Data-Structure/JS/sort/sort-selection-sort.js
+++ Data-Structure/JS/sort/sort-selection-sort.js
@@ -2,6 +2,15 @@
 Selection Sort
 Similar to bubble sort, but instead of first placing large values into sorted position, it places small values into sorted position 
 every time find the smallest, and replace it with the sort start positon, start from index[0]
+
+Selection Sort Pseudocode
+
+Store the first element as the smallest value you've seen so far.
+Compare this item to the next item in the array until you find a smaller number.
+If a smaller number is found, designate that smaller number to be the new "minimum" and continue until the end of the array.
+If the "minimum" is not the value (index) you initially began with, swap the two values.
+Repeat this with the next element until the array is sorted.
+
 */
 const selectionSort = function (arr) {
     if (arr.length < 2) return arr;
