1c33ee9322be64a43c912e71804fdb59672bdd8c
xing wang <uwspstar@gmail.com>
2020-04-05 09:42:22 -0500

    merge sort

--- Data-Structure/JS/sort/sort-merge-sort.js
+++ Data-Structure/JS/sort/sort-merge-sort.js
@@ -12,4 +12,11 @@ In order to implement merge sort, it's useful to first implement a function resp
 Given two arrays which are sorted, this helper function should create a new array which is also sorted, and consists of all of the elements in the two input arrays
 
 This function should run in O(n + m) time and O(n + m) space and should not modify the parameters passed to it.
+
+Merging Arrays Pseudocode
+Create an empty array, take a look at the smallest values in each input array
+While there are still values we haven't looked at...
+    - If the value in the first array is smaller than the value in the second array, push the value in the first array into our results and move on to the next value in the first array
+    - If the value in the first array is larger than the value in the second array, push the value in the second array into our results and move on to the next value in the second array
+    - Once we exhaust one array, push in all remaining values from the other array
 */
