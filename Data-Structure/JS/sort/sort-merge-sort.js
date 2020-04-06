c924a974bce0b4232aa0d4e1539acb6d30d0b39a
xing wang <uwspstar@gmail.com>
2020-04-06 18:54:41 -0500

    merge function

--- Data-Structure/JS/sort/sort-merge-sort.js
+++ Data-Structure/JS/sort/sort-merge-sort.js
@@ -20,3 +20,33 @@ While there are still values we haven't looked at...
     - If the value in the first array is larger than the value in the second array, push the value in the second array into our results and move on to the next value in the second array
     - Once we exhaust one array, push in all remaining values from the other array
 */
+
+const merge = function (arr1, arr2) {
+    let results = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] <= arr2[j]) {
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j]);
+            j++;
+        }
+    }
+
+    while (i < arr1.length) {
+        results.push(arr1[i]);
+        i++;
+    }
+
+    while (j < arr2.length) {
+        results.push(arr2[j]);
+        j++;
+    }
+
+    return results;
+}
+
+console.log(merge([100, 200], [1, 2, 3, 5, 6]))
