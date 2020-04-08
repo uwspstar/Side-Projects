fe64ee6478ffa97aa1b1c4e0e9c6889bd6b060b5
xing wang <uwspstar@gmail.com>
2020-04-08 18:29:01 -0500

    quick sort

---
+++ Data-Structure/JS/sort/sort-quick-sort.js
@@ -0,0 +1,6 @@
+/*
+Quick Sort
+Like merge sort, exploits the fact that arrays of 0 or 1 element are always sorted
+Works by selecting one element (called the "pivot") and finding the index where the pivot should end up in the sorted array
+Once the pivot is positioned appropriately, quick sort can be applied on either side of the pivot
+*/
