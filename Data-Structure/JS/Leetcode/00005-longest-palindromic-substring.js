97ba598860d4901c8931c02c1a966292c7a13d50
xing wang <uwspstar@gmail.com>
2020-04-10 19:11:42 -0500

    longestPalindrome

--- Data-Structure/JS/Leetcode/00005-longest-palindromic-substring.js
+++ Data-Structure/JS/Leetcode/00005-longest-palindromic-substring.js
@@ -1,6 +1,10 @@
 /*
+https://www.youtube.com/watch?v=y2BD4MJqV20
+
 Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
 O(n^2)
+
+
 */
 
 const longestPalindrome = function (s) {
