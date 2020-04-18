e49d05eec63b4df94dc0252ebdd7dd97fd56bf30
xing wang <uwspstar@gmail.com>
2020-04-18 09:44:55 -0500

    coding...

--- Nodejs-Projects/play-ground/login/server.js
+++ Nodejs-Projects/play-ground/login/server.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
 const port = process.env.PORT || 3000;
-const www = process.env.WWW || './';
-app.use(express.static(www));
-console.log(`serving ${www}`);
-app.get('*', (req, res) => {
-    res.sendFile(`index.html`, { root: www });
+app.use(express.json()); // accept json data as input
+
+const users = [
+];
+app.get('/users', (req, res) => {
+    res.json(users)
+    console.log(users);
 });
+
+app.post('/users', async (req, res) => {
+    try {
+        // const salt = await bcrypt.genSalt();
+        // const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const user = { name: req.body.name, password: hashedPassword }
+        users.push(user)
+        res.status(201).send()
+    } catch {
+        res.status(500).send()
+    }
+})
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.name === req.body.name)
+    if (user == null) {
+        return res.status(400).send('Cannot find user')
+    }
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.send('Success')
+        } else {
+            res.send('Not Allowed')
+        }
+    } catch {
+        res.status(500).send()
+    }
+})
+
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
 
