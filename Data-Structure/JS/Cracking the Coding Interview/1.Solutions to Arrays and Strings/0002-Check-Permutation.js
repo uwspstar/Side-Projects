b958d0820bd9e5fa4311c95da5c5606c65a83a8a
xing wang <uwspstar@gmail.com>
2020-05-02 18:43:17 -0500

    Permutation

--- Data-Structure/JS/Cracking the Coding Interview/1.Solutions to Arrays and Strings/0002-Check-Permutation.js
+++ Data-Structure/JS/Cracking the Coding Interview/1.Solutions to Arrays and Strings/0002-Check-Permutation.js
@@ -4,19 +4,60 @@
 
 // A Permutation of a string is another string that contains same characters, only the order of characters can be different. For example, “abcd” and “dabc” are Permutation of each other.
 
-// use sort 
+// use sort , However, if efficiency is very important, we can implement it a different way
+
 const checkPermutations = function (str1, str2) {
+
+    if (str1.length != str2.length) return false;
+
+    let hashmap1 = {};
+    let hashmap2 = {};
+    let len = str1.length;
+
+    for (let i = 0; i < len; i++) {
+        hashmap1[str1[i]] != undefined ? (hashmap1[str1[i]] = hashmap1[str1[i]] + 1) : (hashmap1[str1[i]] = 1);
+
+        hashmap2[str2[i]] != undefined ? (hashmap2[str2[i]] = hashmap2[str2[i]] + 1) : (hashmap2[str2[i]] = 1);
+    }
+    console.log('hashmap1', hashmap1);
+    console.log('hashmap2', hashmap2)
+
+    for (let key in hashmap1) {
+        if (hashmap1.key != hashmap2.key) return false;
+    }
+    return true;
+}
+console.log(checkPermutations("adogs", "agod"));
+
+
+const checkPermutationsJsonStringify = function (str1, str2) {
+
+    if (str1.length != str2.length) return;
+
+    let hashmap1 = {};
+    let hashmap2 = {};
+    let len = str1.length;
+    for (let i = 0; i < len; i++) {
+        hashmap1[str1[i]] != undefined ? (hashmap1[str1[i]] = hashmap1[str1[i]] + 1) : (hashmap1[str1[i]] = 1);
+
+        hashmap2[str2[i]] != undefined ? hashmap2[str2[i]] = hashmap2[str2[i]] + 1 : hashmap2[str2[i]] = 1;
+    }
+    //console.log('hashmap1', hashmap1);
+    //console.log('hashmap2', hashmap2)
+
+    // return JSON.stringify(hashmap1) == JSON.stringify(hashmap2)
+    // not working, becasue the order is different
+
+
+}
+
+
+const checkPermutationswithSort = function (str1, str2) {
     if (str1.length != str2.length) return false;
     let s1 = str1.split('').sort().join('');
     let s2 = str2.split('').sort().join('');
-    return s1 == s2 ? true : false;
+    return s1 == s2;
 }
-
-console.log(checkPermutations("adogs", "agod"))
-console.log(checkPermutations("adogs", "magod"))
-console.log(checkPermutations("adog", "agod"))
-console.log(checkPermutations("dog", "god"))
-console.log(checkPermutations("dogab", "bagod"))
 
 // O(n) isReverseString is subset of the Permutations string
 const isReverseString = function (str1, str2) {
@@ -33,3 +74,10 @@ const isReverseString = function (str1, str2) {
     }
     return true;
 }
+
+
+console.log(checkPermutations("adogs", "agod")) //false
+console.log(checkPermutations("adogs", "magod")) //false
+console.log(checkPermutations("aaaadog", "agodaaa")) //true
+console.log(checkPermutations("doooog", "oogodoo")) //true
+console.log(checkPermutations("dogab", "bagod")) //true
