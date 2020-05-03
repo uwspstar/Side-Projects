64bc49d9deeaf7e8b247ea10842702291328918e
xing wang <uwspstar@gmail.com>
2020-05-02 20:11:36 -0500

    Permutation, using string length reduce

--- Data-Structure/JS/Cracking the Coding Interview/1.Solutions to Arrays and Strings/0002-Check-Permutation.js
+++ Data-Structure/JS/Cracking the Coding Interview/1.Solutions to Arrays and Strings/0002-Check-Permutation.js
@@ -8,7 +8,37 @@
 
 // check with your interviewer about the size of the character set. We assumed that the character set was ASCII
 
+// The slice() method returns a shallow copy of a portion of an array into a new array object selected from begin to end (end not included) where begin and end represent the index of items in that array. *** The original array will not be modified ***.
+
+// The slice() method extracts a section of a string and returns it as a new string, without modifying the original string
+
 const checkPermutations = function (str1, str2) {
+    if (str1.length != str2.length) return false;
+
+    let len = str1.length;
+
+    for (let i = 0; i < len; i++) {
+        //console.log('i = ', i, 'str2.indexOf(str1[i]) = ', str2.indexOf(str1[i]));
+
+        let position = str2.indexOf(str1[i]);
+        if (position < 0) {
+            return false;
+        } else {
+            str2 = str2.slice(0, position) + str2.slice(position + 1, str2.length);
+            //console.log('position =', position, 'str1[i] =', str1[i], 'str2 = ', str2);
+        }
+    }
+    return str2.length === 0;
+}
+
+console.log(checkPermutations("doo", "ood")) //true
+console.log(checkPermutations("adogs", "agod")) //false
+console.log(checkPermutations("adogs", "magod")) //false
+console.log(checkPermutations("aaaadog", "agodaaa")) //true
+console.log(checkPermutations("doooog", "ogodoo")) //true
+console.log(checkPermutations("dogab", "bagod")) //true
+
+const checkPermutationsWithHashMapCheckKey = function (str1, str2) {
 
     if (str1.length != str2.length) return false;
 
@@ -21,16 +51,12 @@ const checkPermutations = function (str1, str2) {
 
         hashmap2[str2[i]] != undefined ? (hashmap2[str2[i]] = hashmap2[str2[i]] + 1) : (hashmap2[str2[i]] = 1);
     }
-    console.log('hashmap1', hashmap1);
-    console.log('hashmap2', hashmap2)
 
     for (let key in hashmap1) {
         if (hashmap1.key != hashmap2.key) return false;
     }
     return true;
 }
-console.log(checkPermutations("adogs", "agod"));
-
 
 const checkPermutationsJsonStringify = function (str1, str2) {
 
@@ -76,10 +102,3 @@ const isReverseString = function (str1, str2) {
     }
     return true;
 }
-
-
-console.log(checkPermutations("adogs", "agod")) //false
-console.log(checkPermutations("adogs", "magod")) //false
-console.log(checkPermutations("aaaadog", "agodaaa")) //true
-console.log(checkPermutations("doooog", "oogodoo")) //true
-console.log(checkPermutations("dogab", "bagod")) //true
