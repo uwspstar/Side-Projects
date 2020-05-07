436b6d148e2bdf7b1625194007a21eaef3ee5e3e
xing wang <uwspstar@gmail.com>
2020-05-06 21:40:55 -0500

    stringCompression

--- Data-Structure/JS/Cracking the Coding Interview/1.Solutions to Arrays and Strings/0006-String-Compression.js
+++ Data-Structure/JS/Cracking the Coding Interview/1.Solutions to Arrays and Strings/0006-String-Compression.js
@@ -1,6 +1,8 @@
 /*
 String Compression: Implement a method to perform basic string compression using the counts of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the "compressed" string would not become smaller than the original string, your method should return the original string. You can assume the string has only uppercase and lowercase letters (a - z).
 At first glance, implementing this method seems fairly straightforward, but perhaps a bit tedious. We iterate through the string, copying characters to a new string and counting the repeats. At each iteration, check if the current character is the same as the next character. If not, add its compressed version to the result.
+
+we can initialize StringBuilder to its necessary capacity up-front. Without this, StringBuilder will (behind the scenes) need to double its capacity every time it hits capacity. The capacity could be double what we ultimately need.
 */
 // It's slow because string concatenation operates in O(n^2 ) time (see StringBuilder on pg 8 ).
 
