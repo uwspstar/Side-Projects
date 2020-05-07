411d83f321065e747d6717baccf9a61cbba1e666
xing wang <uwspstar@gmail.com>
2020-05-06 21:34:47 -0500

    stringCompression

--- Data-Structure/JS/Cracking the Coding Interview/1.Solutions to Arrays and Strings/0006-String-Compression.js
+++ Data-Structure/JS/Cracking the Coding Interview/1.Solutions to Arrays and Strings/0006-String-Compression.js
@@ -2,3 +2,24 @@
 String Compression: Implement a method to perform basic string compression using the counts of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the "compressed" string would not become smaller than the original string, your method should return the original string. You can assume the string has only uppercase and lowercase letters (a - z).
 At first glance, implementing this method seems fairly straightforward, but perhaps a bit tedious. We iterate through the string, copying characters to a new string and counting the repeats. At each iteration, check if the current character is the same as the next character. If not, add its compressed version to the result.
 */
+
+const stringCompression = function (str) {
+    if (str.length < 2) return str;
+
+    let result = '';
+    let count = 1;
+    
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == str[i + 1]) {
+            count++;
+        } else { 
+            result = result + str[i] + count;
+            count = 1;
+        }
+    }
+    return result;
+
+}
+
+console.log(stringCompression('abc')) //a1blc1
+console.log(stringCompression('aabcccccaaa')) //a2blc5a3
