b66304e999b69f88f0548c1cb12e5719dbe771cc
xing wang <uwspstar@gmail.com>
2020-05-10 15:24:01 -0500

    two pointers

---
+++ Data-Structure/JS/Leetcode/two pointers technique/max-fix-size.js
@@ -0,0 +1,12 @@
+/*
+arr = [1,3,5,8,0,2,4,9,-7]
+fix size = 3
+max is 16 = [3,5,8]
+*/
+
+const maxFixSize = function (arr, size) {
+
+}
+
+console.log(maxFixSize([1,3,5,8,0,2,4,9,7], 3))
+console.log(maxFixSize([1,3,5,8,0,2,4,9,7], 4))
