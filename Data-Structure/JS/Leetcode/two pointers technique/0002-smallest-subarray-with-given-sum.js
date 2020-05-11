8b0a7777a365a7be4872b471a3d70b8c92246f95
xing wang <uwspstar@gmail.com>
2020-05-10 21:39:06 -0500

    list

--- Data-Structure/JS/Leetcode/two pointers technique/0002-smallest-subarray-with-given-sum.js
+++ Data-Structure/JS/Leetcode/two pointers technique/0002-smallest-subarray-with-given-sum.js
@@ -2,8 +2,15 @@
 */
 
 const smallestSubarrayWithGivenSum = function (arr, sum) {
+    let smallest_array_size = 1;
 
-    smallestSubarrayWithGivenSum
+    let smallest = Number.MAX_SAFE_INTEGER;
+    let current = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        current += arr[i];
+        if (current)
+    }
 }
 
 console.log(smallestSubarrayWithGivenSum([1, 3, 5, 8, 0, 2, 4, 9, -7], 1))
