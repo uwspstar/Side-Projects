3c5265ebc0435b897e7c53d1188deb54378ffdbc
xing wang <uwspstar@gmail.com>
2020-05-23 18:44:19 -0500

    join

--- Python-Projects/play-ground/basic/list.py
+++ Python-Projects/play-ground/basic/list.py
@@ -13,10 +13,17 @@ doubled_numbers = [num * 2 for num in numbers]
 print(doubled_numbers)  # [2, 4, 6, 8, 10]
 
 name = 'ab'
-[char.upper() for char in name] # ['A', 'B']
+[char.upper() for char in name]  # ['A', 'B']
 
 friends = ['ashley', 'matt', 'michael']
-[friend[0].upper() for friend in friends] # ['Ashley', 'Matt', 'Michael']
+[friend[0].upper() for friend in friends]  # ['Ashley', 'Matt', 'Michael']
+
+[num*10 for num in range(1, 6)]  # [10, 20, 30, 40, 50]
+[bool(val) for val in [0, [], '']]  # [False, False, False]
+
+numbers = [1, 2, 3, 4, 5]
+string_list = [str(num) for num in numbers]
+print(string_list)  # ['1', '2', '3', '4', '5']
 
 # Tricks with Slices
 string = "This is fun!"
