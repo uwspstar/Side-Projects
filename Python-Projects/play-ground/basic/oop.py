cecd57a5f2d9548e571d320e91e1de381df6f304
xing wang <uwspstar@gmail.com>
2020-05-23 07:44:54 -0500

    oop

--- Python-Projects/play-ground/basic/oop.py
+++ Python-Projects/play-ground/basic/oop.py
@@ -1,2 +1,13 @@
 class user:
     pass
+
+# Classes in Python can have a special __init__ method, which gets called every time you create an instance of the class (instantiate).
+
+class vehicle:
+
+    def __init__ (self, year, color, brand):
+        self.year = year
+        self.color = color
+        self.brand = brand
+
+
