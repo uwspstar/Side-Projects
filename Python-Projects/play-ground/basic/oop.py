d6af3c3b21563270fb9bfd9a90c5b094c48ce92c
xing wang <uwspstar@gmail.com>
2020-05-23 09:00:26 -0500

    oop

--- Python-Projects/play-ground/basic/oop.py
+++ Python-Projects/play-ground/basic/oop.py
@@ -10,6 +10,10 @@ print(type(user1))
 # The self keyword refers to the current class instance. self must always be the first parameter to __init__ and any methods and properties on class instances.
 # Class methods are methods (with the @classmethod decorator) that are not concerned with instances, but the class itself.
 # The first argument is cls (for class) instead of self. Like self, it does not need to be passed in explicitly.
+# Class methods are available on the class itself and any instances of the class, and are mostly used for building new instances of classes
+# In Python, inheritance works by passing the parent class as an argument to the definition of a child class:
+# The super() keyword allows us to call the __init__ function of a parent class
+
 
 class Vehicle:
 
@@ -20,16 +24,28 @@ class Vehicle:
 
     def get_year(self):
         print(self.year)
-        
 
     @classmethod
     def get_year_from_default(cls, year):
-        return cls(year)  # this is the same as calling Person(*params)
+        pass
+        # return cls(*params)  # this is the same as calling Person(*params)
 
 
+class Truck(Vehicle):
+    def __init__(self, name):
+        super().__init__("1973", "while", "GM")
+        self.name = name
 
-v = Vehicle("1973", "red", "BMW") # Creating an object that is an instance of a class is called instantiating a class.
+
+# Creating an object that is an instance of a class is called instantiating a class.
+v = Vehicle("1973", "red", "BMW")
 print(type(v))
 print(v.brand, v.color, v.year)
 print(v.get_year())
 print(Vehicle.get_year_from_default("1988"))
+
+t = Truck("big")
+print(type(t))
+print(t.brand, t.color, t.year)
+print(t.name, t.brand, t.color, t.year) # big GM while 1973
+print(t.get_year())
