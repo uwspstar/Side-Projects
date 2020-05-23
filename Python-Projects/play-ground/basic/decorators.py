7b1b0713c019a970211af071822247b1af9d7e6d
xing wang <uwspstar@gmail.com>
2020-05-23 18:26:10 -0500

    join

--- Python-Projects/play-ground/basic/decorators.py
+++ Python-Projects/play-ground/basic/decorators.py
@@ -1,3 +1,4 @@
+"""
 def be_polite(fn):
     def wrapper():
         print("What a pleasure to meet you!")
@@ -11,3 +12,36 @@ def greet():
 greet = be_polite(greet)
 # we are decorating our function 
 # with politeness!
+"""
+
+
+def be_polite(fn):
+    def wrapper():
+        print("What a pleasure to meet you!")
+        fn()
+        print("Have a great day!")
+    return wrapper
+
+
+@be_polite
+def greet():
+    print("My name is Matt.")
+
+# we don't need to set
+# greet = be_polite(greet)
+
+
+def shout(fn):
+    def wrapper(name):
+        return fn(name).upper()
+    return wrapper
+
+
+@shout
+def greet(name):
+    return f"Hi, I'm {name}."
+
+
+@shout
+def order(main, side):
+    return f"Hi, I'd like the {main}, with a side of {side}, please."
---
+++ Python-Projects/play-ground/basic/list.py
@@ -0,0 +1,18 @@
+numbers = [5, 6, 7, 8, 9, 10]
+# index
+numbers.index(6)  # 1
+numbers.index(9)  # 4
+
+# reverse
+first_list = [1, 2, 3, 4]
+first_list.reverse()
+print(first_list) # [4, 3, 2, 1]
+
+# sort
+another_list = [6, 4, 1, 2, 5]
+another_list.sort()
+print(another_list) # [1, 2, 4, 5, 6]
+
+# join
+words = ['Coding', 'Is', 'Fun!']
+' '.join(words) # 'Coding is Fun!'
