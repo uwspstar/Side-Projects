2e8e938908e7515e3396d116e4d8e7d8a30f5906
xing wang <uwspstar@gmail.com>
2020-05-24 07:35:46 -0500

    dictionary

--- Python-Projects/play-ground/basic/dictionaries.py
+++ Python-Projects/play-ground/basic/dictionaries.py
@@ -1,3 +1,6 @@
+# Python : Dictionary keys are almost always numbers or strings, values can be anything!
+# JS : The Map object holds key-value pairs and remembers the original insertion order of the keys. Any value (both objects and primitive values) may be used as either a key or a value
+
 instructor = {
     "name": "Colt",
     "owns_dog": True,
