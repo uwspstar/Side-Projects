41bc391d1b7a83f18873138c94f317a55eea4fa0
xing wang <uwspstar@gmail.com>
2020-05-24 07:32:32 -0500

    dictionary

---
+++ Python-Projects/play-ground/basic/dictionaries.py
@@ -0,0 +1,8 @@
+instructor = {
+    "name": "Colt",
+    "owns_dog": True,
+    "num_courses": 4,
+    "favorite_language": "Python",
+    "is_hilarious": False,
+    44: "my favorite number!"
+}
--- Python-Projects/play-ground/basic/list.py
+++ Python-Projects/play-ground/basic/list.py
@@ -1,7 +1,25 @@
+board = [[num for num in range(1, 4)] for val in range(1, 4)]
+print(board)  # [[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+
+[["X" if num % 2 != 0 else "O" for num in range(1, 4)] for val in range(1, 4)]
+# [['X', 'O', 'X'], ['X', 'O', 'X'], ['X', 'O', 'X']]
+
+# Swapping Values
+names = ["James", "Michelle"]
+names[0], names[1] = names[1], names[0]
+print(names) # ['Michelle', 'James']
+
+
 # Nested Lists
 nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 nested_list[0][1]  # 2
 nested_list[1][-1]  # 6
+
+for l in nested_list:
+    for val in l:
+        print(val)
+
+[[print(val) for val in l] for l in nested_list]
 
 # List Comprehension vs Looping
 numbers = [1, 2, 3, 4, 5]
