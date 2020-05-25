883e844f04db4ba84c5201901b20ccb79cf702bb
xing wang <uwspstar@gmail.com>
2020-05-25 11:59:37 -0500

    linked list

---
+++ Data-Structure/JS/Linked List/push.js
@@ -0,0 +1,39 @@
+/*
+This function should accept a value
+Create a new node using the value passed to the function
+If there is no head property on the list, set the head and tail to be the newly created node
+Otherwise set the next property on the tail to be the new node and set the tail property on the list to be the newly created node
+Increment the length by one
+Return the linked list
+*/
+
+// piece of data - val
+//reference to next node - next
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val) {
+
+    }
+}
+
+// var first = new Node("Hi")
+// first.next = new Node("there")
+// first.next.next = new Node("how")
+// first.next.next.next = new Node("are")
+// first.next.next.next.next = new Node("you")
+
+var list = new SinglyLinkedList()
+list.push("HELLO")
+list.push("GOODBYE")
