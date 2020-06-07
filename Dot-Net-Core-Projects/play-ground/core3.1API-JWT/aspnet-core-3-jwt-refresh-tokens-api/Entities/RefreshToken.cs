76d14f241122da67584656530d47d3a97741d2d4
xing wang <uwspstar@gmail.com>
2020-06-06 19:58:45 -0500

    .net core

---
+++ Dot-Net-Core-Projects/play-ground/core3.1API-JWT/aspnet-core-3-jwt-refresh-tokens-api/Entities/RefreshToken.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Entities
+{
+    [Owned]
+    public class RefreshToken
+    {
+        public string Token { get; set; }
+        public DateTime Expires { get; set; }
+        public bool IsExpired => DateTime.UtcNow >= Expires;
+        public DateTime Created { get; set; }
+        public string CreatedByIp { get; set; }
+        public DateTime? Revoked { get; set; }
+        public string RevokedByIp { get; set; }
+        public string ReplacedByToken { get; set; }
+        public bool IsActive => Revoked == null && !IsExpired;
+    }
+}
