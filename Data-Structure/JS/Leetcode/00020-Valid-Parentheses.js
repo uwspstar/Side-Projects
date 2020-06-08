24de0feee7acb7d1875707ae9212c050909b45e7
xing wang <uwspstar@gmail.com>
2020-06-07 22:08:18 -0500

    valide-parentheses

--- Data-Structure/JS/Leetcode/00020-Valid-Parentheses.js
+++ Data-Structure/JS/Leetcode/00020-Valid-Parentheses.js
@@ -24,26 +24,75 @@ Example 5: Input: "{[]}" Output: true
 */
 
 const validParentheses = function (str) {
+
+    if (str.length == 0) return true;
+    if (str.length % 2 != 0) return false;
+
     let stack = []
+
     for (let i = 0; i < str.length; i++) {
+        console.log('str[i] = ', str[i])
+        console.log('str[i-1] = ', str[i - 1])
+
         if (str[i] === '(' || str[i] === '{' || str[i] === '[') {
             stack.push(str[i])
-        } else if (str[i] === ')') {
-            if (str[i - 1] !== '(') return false
+        }
+
+        if (str[i] === ')') {
+            if (stack[stack.length - 1] !== '(') return false
             else stack.pop('(')
-        } else if (str[i] === '}') {
-            if (str[i - 1] !== '{') return false
+        }
+
+        if (str[i] === '}') {
+            if (stack[stack.length - 1] !== '{') return false
             else stack.pop('{')
-        } else if (str[i] === ']') {
-            if (str[i - 1] !== '[') return false
+        }
+
+        if (str[i] === ']') {
+            if (stack[stack.length - 1] !== '[') return false
             else stack.pop('[')
         }
-        //console.log(stack)
+
+        console.log('stack = ', stack)
     }
-    return stack.length === 0
+    console.log('stack.length', stack.length)
+    return !stack.length 
 }
 
+console.log(validParentheses("{[]}")) //true
+/*
 console.log(validParentheses("()[]{}")) //true
 console.log(validParentheses("([)]")) // false
 console.log(validParentheses("][")) // false
-console.log(validParentheses("]()()[")) // false
+console.log(validParentheses("]()()[")) //false
+*/
+
+
+var isValid = function(s) {
+    
+    if(s.length == 0) return true;
+    if(s.length % 2 != 0) return false;
+    
+    let arr = s.split('');
+    let stack = [];
+    
+    for(let i = 0 ; i < arr.length; i++) {
+        
+        if (arr[i] === '(' || arr[i] ==='{' || arr[i] ==='['){
+            stack.push(arr[i]);
+        } else if ( arr[i] === ')') {
+            if(stack[stack.length - 1] !== '(') return false;
+            stack.pop();
+        } else if ( arr[i] === '}') {
+            if(stack[stack.length-1] !== '{') return false;
+            stack.pop();
+        } else if ( arr[i] === ']') {
+            if(stack[stack.length-1] !== '[') return false;
+            stack.pop();
+        }   
+    }
+    
+     return !stack.length;
+    
+};
+console.log(isValid("{[]}")) //true
