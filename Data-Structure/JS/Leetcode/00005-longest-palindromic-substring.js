9867662eb893f27012fb5143ac416a3a069a25f9
xing wang <uwspstar@gmail.com>
2020-06-13 04:48:57 -0500

    longestPalindrome

--- Data-Structure/JS/Leetcode/00005-longest-palindromic-substring.js
+++ Data-Structure/JS/Leetcode/00005-longest-palindromic-substring.js
@@ -5,13 +5,23 @@ https://www.youtube.com/watch?v=m2Mk9JN5T4A&list=PLvyIyKZVcfAk4vxVK-QQYha7VfE4SL
 Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
 O(n^2)
 
+5. Longest Palindromic Substring
+Medium  
 
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example 1: Input: "babad" Output: "bab"
+Note: "aba" is also a valid answer. 
+
+Example 2: Input: "cbbd" Output: "bb"
 */
 
 const longestPalindrome = function (str) {
 
 };
 
+console.log(longestPalindrome("cbbd")) //babab
 console.log(longestPalindrome("racecar")) //racecar
 console.log(longestPalindrome("babad")) //bab
 console.log(longestPalindrome("babab")) //babab
+console.log(longestPalindrome("ababbad")) //abba
