456515775f11f757d1dc044936078b405a57e61b
xing wang <uwspstar@gmail.com>
2020-06-14 16:00:48 -0500

    data

---
+++ Data-Structure/JS/Frequency Counter/test.js
@@ -0,0 +1,10 @@
+// q1 : has negative number inside array ?
+// 
+
+const same = function (arr1, arr2) {
+
+}
+
+console.log(same([1,2,3],[4,1,9]))
+console.log(same([1,2,3],[1,9]))
+console.log(same([1,2,1],[4,4,1]))
