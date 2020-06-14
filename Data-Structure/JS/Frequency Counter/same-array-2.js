f71adb993cf8aa5895ffb4b6c16dd7b42cf8e0be
xing wang <uwspstar@gmail.com>
2020-06-14 17:47:11 -0500

    data

---
+++ Data-Structure/JS/Frequency Counter/same-array-2.js
@@ -0,0 +1,7 @@
+const same = function (arr1, arr2) {
+
+}
+
+console.log(same([1, 2, 3], [4, 1, 9]))
+console.log(same([1, 2, 3], [1, 9]))
+console.log(same([1, 2, 1], [4, 4, 1]))
--- Data-Structure/JS/Frequency Counter/test.js
+++ Data-Structure/JS/Frequency Counter/test.js
@@ -1,21 +1,22 @@
 // q1 : has negative number inside array ?
-// Array.prototype.indexOf()
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
-// splice vs slice
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+// using new Map() instead of using {}
+// But the plain object has a limitation: its keys have to be strings (or rarely used symbols).
+
+// https://dmitripavlutin.com/maps-vs-plain-objects-javascript/
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+
 
 const same = function (arr1, arr2) {
     if (arr1.length != arr2.length) return false
 
+    let obj1 = new Map()
+    let obj2 = new Map()
+
     for (let i = 0; i < arr1.length; i++) {
-        let rightIndex = arr2.indexOf(arr1[i] ** 2)
-        if (rightIndex < 0) return false
-        arr2.splice(rightIndex, 1)
+
     }
-    return true
 }
 
-console.log(same([1, 2, 3], [4, 1, 9]))
-console.log(same([1, 2, 3], [1, 9]))
-console.log(same([1, 2, 1], [4, 4, 1]))
+console.log(same([1, 2, 3], [4, 1, 9])) // true
+console.log(same([1, 2, 3], [1, 9])) // false
+console.log(same([1, 2, 1], [4, 4, 1])) // false
