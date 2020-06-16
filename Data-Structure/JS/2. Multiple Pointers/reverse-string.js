e531620d43b86c21d5d1fe512ff7c7e0bf962b5e
xing wang <uwspstar@gmail.com>
2020-06-16 14:23:30 -0500

    data

--- Data-Structure/JS/2. Multiple Pointers/reverse-string.js
+++ Data-Structure/JS/2. Multiple Pointers/reverse-string.js
@@ -1,13 +1,13 @@
 const reverseStr = function (str) {
     // O(n)
-    if (str.length < 2) return str;
-    let arr = str.split('');
-    let i = 0;
-    let j = arr.length;
+    if (str.length < 2) return str
+    let arr = str.split('')
+    let i = 0
+    let j = arr.length
     while (i < j) {
-        [arr[i], arr[j]] = [arr[j], arr[i]];
-        i++;
-        j--;
+        [arr[i], arr[j]] = [arr[j], arr[i]]
+        i++
+        j--
     }
     return arr.join('')
 }
