18271c921c8131d2bd342e31ca1b7228e0a67575
xing wang <uwspstar@gmail.com>
2020-06-16 20:25:16 -0500

    maxSubArraySum

--- Data-Structure/JS/3. Sliding Window Pattern/test.js
+++ Data-Structure/JS/3. Sliding Window Pattern/test.js
@@ -1,9 +1,15 @@
 const maxSubArraySum = function (arr, num) {
 
-        if (arr.length < num) return null
-        let max = -Infinity
-        for (let i = 0; i < arr.length - m ; i++ )
-
+    if (arr.length < num) return null
+    let max = -Infinity
+    for (let i = 0; i < arr.length - num + 1; i++) {
+        let temp = 0
+        for (let j = 0; j < num; j++) {
+            temp += arr[i + j]
+        }
+        max = Math.max(temp, max)
+    }
+    return max
 }
 
 console.log(maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 2))
