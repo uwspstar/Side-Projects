cd4ac44040a96222c39239ef3e6570f181767c65
xing wang <uwspstar@gmail.com>
2020-06-18 18:59:58 -0500

    maxSubarraySum

--- Data-Structure/JS/3. Sliding Window Pattern/test.js
+++ Data-Structure/JS/3. Sliding Window Pattern/test.js
@@ -1,7 +1,14 @@
-//slide window
-//O(n)
-const maxSubArraySum = function (arr, num) {
+/*
+Sliding Window - maxSubarraySum
+Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function.
 
+Note that a subarray must consist of consecutive elements from the original array. In the first example below, [100, 200, 300] is a subarray of the original array, but [100, 300] is not.
+
+Constraints: Time Complexity - O(N) Space Complexity - O(1)
+
+*/
+
+const maxSubarraySum = function (arr, num) {
     if (arr.length < num) return null
     let max = 0
     let temp = 0
@@ -9,15 +16,19 @@ const maxSubArraySum = function (arr, num) {
     for (let i = 0; i < num ; i++) {
         max += arr[i]
     }
+    console.log(max)
+
     for (let i = num; i < arr.length; i++) {
         temp = max + arr[i] - arr[i - num]
+        console.log('i =',i, 'temp = ', temp,'arr[i] = ', arr[i], 'arr[i - num] =', arr[i - num])
+
         max = Math.max(temp, max)
     }
     return max
 }
+console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1], 2)) // 5
 
-console.log(maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 2)) // 10
-console.log(maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 4)) // 17
-console.log(maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 1)) // 8
-console.log(maxSubArraySum([4, 2, 1, 6, 2], 4)) //13
-console.log(maxSubArraySum([], 4)) // null
+// console.log(maxSubarraySum([100,200,300,400], 2)) // 700
+// console.log(maxSubarraySum([1,4,2,10,23,3,1,0,20], 4))  // 39 
+// console.log(maxSubarraySum([-3,4,0,-2,6,-1], 2)) // 5
+// console.log(maxSubarraySum([2,3], 3)) // null
