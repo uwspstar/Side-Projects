5e339a845c5054b9f6537c013867b6a6780f2638
xing wang <uwspstar@gmail.com>
2020-06-17 21:00:02 -0500

    isSubsequence

--- Data-Structure/JS/2. Multiple Pointers/is-Subsequence-1.js
+++ Data-Structure/JS/2. Multiple Pointers/is-Subsequence-1.js
@@ -13,7 +13,7 @@ const isSubsequence = function (str1, str2) {
     let j = 0
     while (j < str2.length) {
         if (str1[i] === str2[j]) i++ 
-        if (i === str1.length) return true
+        if (i === str1.length) return true //str1.length because last i++
         j++
     }
     return false
@@ -24,16 +24,4 @@ console.log(isSubsequence('sing', 'sting')) // true
 console.log(isSubsequence('abc', 'abracadabra')) // true
 console.log(isSubsequence('abc', 'acb')) // false (order matters)
 
-/*
-function isSubsequence(str1, str2) {
-  var i = 0;
-  var j = 0;
-  if (!str1) return true;
-  while (j < str2.length) {
-    if (str2[j] === str1[i]) i++;
-    if (i === str1.length) return true;
-    j++;
-  }
-  return false;
-}
-*/
+  
