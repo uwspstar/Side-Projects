585e0a0d720f35403a6fe0da4ade1593cd9c1ffb
xing wang <uwspstar@gmail.com>
2020-06-18 19:12:34 -0500

    maxSubarraySum

--- Data-Structure/JS/3. Sliding Window Pattern/test.js
+++ Data-Structure/JS/3. Sliding Window Pattern/test.js
@@ -20,15 +20,32 @@ const maxSubarraySum = function (arr, num) {
 
     for (let i = num; i < arr.length; i++) {
         temp = max + arr[i] - arr[i - num]
-        console.log('i =',i, 'temp = ', temp,'arr[i] = ', arr[i], 'arr[i - num] =', arr[i - num])
+        console.log('i =',i,'max =', max, 'temp = ', temp,'arr[i] = ', arr[i], 'arr[i - num] =', arr[i - num])
 
         max = Math.max(temp, max)
     }
     return max
 }
-console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1], 2)) // 5
+console.log(maxSubarraySum([3, -2, 7, -4, 1, -1], 2)) // 5
 
 // console.log(maxSubarraySum([100,200,300,400], 2)) // 700
 // console.log(maxSubarraySum([1,4,2,10,23,3,1,0,20], 4))  // 39 
 // console.log(maxSubarraySum([-3,4,0,-2,6,-1], 2)) // 5
 // console.log(maxSubarraySum([2,3], 3)) // null
+
+/*
+function maxSubarraySum(arr, num){
+    if (arr.length < num) return null;
+ 
+    let total = 0;
+    for (let i=0; i<num; i++){
+       total += arr[i];
+    }
+    let currentTotal = total;
+    for (let i = num; i < arr.length; i++) {
+       currentTotal += arr[i] - arr[i-num];
+       total = Math.max(total, currentTotal);
+    }
+    return total;
+}
+*/
