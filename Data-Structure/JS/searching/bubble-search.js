3ac170ff09ab3b3d4546557a68ae1563ead0701c
xing wang <uwspstar@gmail.com>
2020-06-23 18:53:00 -0500

    sortBits

---
+++ Data-Structure/JS/searching/bubble-search.js
@@ -0,0 +1,18 @@
+/*
+
+https://medium.com/@mera.stackhouse/which-sorting-algorithms-to-know-for-the-tech-interview-654a1f619e1d
+
+do
+
+  swapped = false
+
+  for i = 1 to indexOfLastUnsortedElement-1
+
+    if leftElement > rightElement
+
+      swap(leftElement, rightElement)
+
+      swapped = true
+
+while swapped
+*/
