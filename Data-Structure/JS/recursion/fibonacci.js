20831d3b5749dd1e02096161695f6efd27ce0421
xing wang <uwspstar@gmail.com>
2020-06-27 07:54:46 -0500

    top 50

--- Data-Structure/JS/recursion/fibonacci.js
+++ Data-Structure/JS/recursion/fibonacci.js
@@ -34,3 +34,20 @@ const fibonacciArray = function (n) {
     }
     return result
 }
+
+const fibonacciArray = function (n) {
+    if (n < 2) return [1] // n = 1, one element
+    if (n < 3) return [1, 1] // n = 2 , return [1, 1]
+    let result = [1, 1]
+    function help(num) {
+        for (let i = 2; i < num; i++) {
+            result.push(result[i - 1] + result[i - 2])
+        }
+    }
+    help(n)
+    return result
+}
+
+console.log(fibonacciArray(3)) // [ 1, 1, 2 ]
+console.log(fibonacciArray(4)) // [ 1, 1, 2, 3 ]
+console.log(fibonacciArray(5)) // [ 1, 1, 2, 3, 5 ]
--- Data-Structure/JS/recursion/test.js
+++ Data-Structure/JS/recursion/test.js
@@ -1,4 +1,22 @@
 
+const fibonacciArray = function (n) {
+    if (n < 2) return [1] // n = 1, one element
+    if (n < 3) return [1, 1] // n = 2 , return [1, 1]
+    let result = [1, 1]
+    function help(num) {
+        for (let i = 2; i < num; i++) {
+            result.push(result[i - 1] + result[i - 2])
+        }
+    }
+    help(n)
+    return result
+}
+
+console.log(fibonacciArray(3)) // [ 1, 1, 2 ]
+console.log(fibonacciArray(4)) // [ 1, 1, 2, 3 ]
+console.log(fibonacciArray(5)) // [ 1, 1, 2, 3, 5 ]
+
+/*
 // 4! = 4 * 3 * 2 * 1
 const factorial = function (n) {
     if (n === 1) return 1
@@ -7,7 +25,7 @@ const factorial = function (n) {
 console.log(factorial(3)) // 6
 console.log(factorial(4)) // 24
 
-/*
+
 const fibonacci = function (n) {
     if (n < 3) return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
