78684f8eb9c8bda65d40b31255d78026e33bba2c
xing wang <uwspstar@gmail.com>
2020-07-03 11:22:24 -0500

    sort

--- Data-Structure/JS/tips/is-Even-Odd.js
+++ Data-Structure/JS/tips/is-Even-Odd.js
@@ -5,3 +5,12 @@ function isEvenOrOdd(n) {
 function isEvenOrOdd(n) {
     return n & 1 ? 'Odd' : 'Even';
 }
+
+function isEven(int) {
+    return (int & 1) === 0;
+}
+
+console.log(isOdd(34)); // false
+console.log(isOdd(-63)); // true
+console.log(isEven(-12)); // true
+console.log(isEven(199)); // false
