619f0846029777281d1429b358fd46aa9d190485
xing wang <uwspstar@gmail.com>
2020-07-04 09:04:08 -0500

    search

---
+++ Data-Structure/JS/searching/search-Math-Str.js
@@ -0,0 +1,27 @@
+// two pointer
+// O(N^2)
+// ("lorie loled", "lol")
+const searchMathStr = function (long, short) {
+    let count = 0;
+    for (let i = 0; i < long.length; i++) {
+        for (let j = 0; j < short.length; j++) {
+            if (short[j] !== long[i + j]) break;
+            if (j === short.length - 1) count++; //find one math increase
+        }
+    }
+    return count;
+}
+
+
+function naiveSearch(long, short) {
+    var count = 0;
+    for (var i = 0; i < long.length; i++) {
+        for (var j = 0; j < short.length; j++) {
+            if (short[j] !== long[i + j]) break; //restart again
+            if (j === short.length - 1) count++;
+        }
+    }
+    return count;
+}
+
+naiveSearch("lorie loled", "lol")
--- Data-Structure/JS/searching/test.js
+++ Data-Structure/JS/searching/test.js
@@ -1,4 +1,18 @@
-// Refactored Version
+// ("lorie loled", "lol")
+const searchMathStr = function (long, short) {
+    let count = 0;
+    for (let i = 0; i < long.length; i++) {
+        for (let j = 0; j < short.length; j++) {
+            console.log('i=',i,'j=',j,'short[j]=', short[j],'long[i + j]=',long[i + j])
+            if (short[j] !== long[i + j]) break;
+            if (j === short.length - 1) count++;// find one math increase
+        }
+    }
+    return count;
+}
+console.log("lorie loled", "lol")
+
+/*
 
 function binarySearch(arr, num) {
     let start = 0
@@ -14,3 +28,5 @@ function binarySearch(arr, num) {
 
 console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 13))
 console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 103))
+
+*/
