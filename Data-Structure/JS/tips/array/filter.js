10d18cfb239f13e0ed8c9b0632a433d6f27d62af
xing wang <uwspstar@gmail.com>
2020-07-05 15:25:27 -0500

    sort

---
+++ Data-Structure/JS/tips/array/filter.js
@@ -0,0 +1,4 @@
+//Array.prototype.filter()
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+//creates a new array with all elements that pass the test implemented by the provided function.
+// let newArray = arr.filter(callback(element[, index, [array]])[, thisArg])
