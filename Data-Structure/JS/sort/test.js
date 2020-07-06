a4755f71f978cea9af07f92c718a9440a932f18f
xing wang <uwspstar@gmail.com>
2020-07-06 06:39:09 -0500

    sort

--- Data-Structure/JS/sort/test.js
+++ Data-Structure/JS/sort/test.js
@@ -1,6 +1,21 @@
 const insertionSort = function (arr) {
-    
+    if (arr.length < 2) return arr
+    //[3, 6, 4] 4 is current
+    //left is sorted
+    for (let i = 1; i < arr.length; i++) {
+        let current = arr[i]
+        let j = i - 1
+        for (; j >= 0; j--) {
+            if (arr[j] > arr[j + 1]) {
+                arr[j + 1] = arr[j]
+            } else break;
+        }
+        arr[j + 1] = current
+    }
+    return arr
 }
+
+
 
 /*
 // find the smallest item index, and swap later
