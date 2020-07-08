244840043ee4328b8d94e06a2a269eb3468ecc12
xing wang <uwspstar@gmail.com>
2020-07-08 16:07:37 -0500

    search

--- Data-Structure/JS/searching/Find-the-Missing-Number.js
+++ Data-Structure/JS/searching/Find-the-Missing-Number.js
@@ -12,14 +12,14 @@ Explanation: The missing number from 1 to 5 is 4
 
 // 1: ask if the arr is sorted, if it is sorted, O(N) + O(N)
 const findMissingNumberSorted = function (arr) {
-    if (arr.length === 0) return 1
+    if (arr.length === 0) return 1;
     //if (arr.length === 1) return arr[0] === 1 ? 0 : 1 
-    let arrSort = arr.sort()  // ON(N)
-    if (arrSort[0] !== 1) return 1
+    let arrSort = arr.sort();  // ON(N)
+    if (arrSort[0] !== 1) return 1;
     for (let i = 1; i < arrSort.length; i++) {
-        if (arrSort[i] !== (i + 1)) return arrSort[i - 1] + 1
+        if (arrSort[i] !== (i + 1)) return arrSort[i - 1] + 1;
     }
-    return 1
+    return 1;
 }
 console.log(findMissingNumberSorted([1, 2, 4, 6, 3, 7, 8]))
 
