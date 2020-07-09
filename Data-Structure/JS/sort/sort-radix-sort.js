b600727cd10fdae651a17d21691e94efa12a5f6d
xing wang <uwspstar@gmail.com>
2020-07-09 06:57:57 -0500

    Radix Sort

--- Data-Structure/JS/sort/sort-radix-sort.js
+++ Data-Structure/JS/sort/sort-radix-sort.js
@@ -1,6 +1,8 @@
 //https://brilliant.org/wiki/radix-sort/#
 /*
-Radix sort is a special sorting algorithm that works on lists of numbers
+Radix sort is a special sorting algorithm that works on lists of "NUMBERS"
+It NEVER makes comparisons between elements!
+It exploits the fact that information about the size of a number is encoded in the number of digits.
 */
 
 
