739f43375a6b63d79ef3ea6b7986c6f724a97f3b
xing wang <uwspstar@gmail.com>
2020-07-09 08:28:35 -0500

    tip

---
+++ Data-Structure/JS/tips/array/from.js
@@ -0,0 +1,13 @@
+/*
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+
+Array.from() static method creates a new, shallow-copied Array instance from an array-like or iterable object.
+
+Array.from() lets you create Arrays from:
+
+array-like objects (objects with a length property and indexed elements); or
+iterable objects (objects such as Map and Set).
+
+The length property of the from() method is 1.
+*/
+//Array.from(arrayLike [, mapFn [, thisArg]])
