cd7d67321fc9f509721a047cf53b3cd0acda2072
xing wang <uwspstar@gmail.com>
2020-07-09 08:05:09 -0500

    Radix Sort

--- Data-Structure/JS/sort/js-sort-algorithem.js
+++ Data-Structure/JS/sort/js-sort-algorithem.js
@@ -52,5 +52,12 @@ Insertion Sort - O(n^2)
 Selection Sort - O(n^2)
 Quick Sort - O(n log (n))
 Merge Sort - O(n log (n))
+Radix sort - O(nk)
+
+- Merge sort and quick sort are standard efficient sorting algorithms
+- Quick sort can be slow in the worst case, but is comparable to merge sort on average
+- Merge sort takes up more memory because it creates a new array (in-place merge sorts exist, but they are really complex!)
+- Radix sort is a fast sorting algorithm for numbers
+- Radix sort exploits place value to sort numbers in linear time (for a fixed number of digits)
 
 */
---
+++ Data-Structure/JS/tips/string/charCodeAt.js
@@ -0,0 +1,4 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charCodeAt
+
+// str.charCodeAt(index)
+//'ABC'.charCodeAt(0)  // returns 65
