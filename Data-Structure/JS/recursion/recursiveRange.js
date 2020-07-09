402a5d5f6c77e153756117b9a0e034fcd6a27216
xing wang <uwspstar@gmail.com>
2020-07-08 21:31:35 -0500

    searchMatchStr

--- Data-Structure/JS/recursion/recursiveRange.js
+++ Data-Structure/JS/recursion/recursiveRange.js
@@ -3,11 +3,11 @@
 // recursiveRange(10) // 55 = 10 + ... + 1
 
 function recursiveRange(n) {
-    if (n < 0) return undefined
-    if (n === 0) return 0
+    if (n < 0) return undefined;
+    if (n === 0) return 0;
     return n + recursiveRange(n - 1)
 }
 
-console.log(recursiveRange(6))
-console.log(recursiveRange(10))
+console.log(recursiveRange(6));
+console.log(recursiveRange(10));
  
