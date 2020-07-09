f7ba21c706c5461b594d9f3c229adc2f69146d2b
xing wang <uwspstar@gmail.com>
2020-07-09 09:01:57 -0500

    tip

--- Data-Structure/JS/tips/js-bitwise-operation.js
+++ Data-Structure/JS/tips/js-bitwise-operation.js
@@ -1,6 +1,8 @@
 // https://blog.logrocket.com/interesting-use-cases-for-javascript-bitwise-operators/
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Bitwise_Operators
+
 /*
+------------------------------------------------------------------------
 Operator	    Description	Example	        Same as	Result	Decimal
 &	AND	        x = 5 & 1	0101 & 0001	    0001	 1
 |	OR	        x = 5 | 1	0101 | 0001	    0101	 5
@@ -8,8 +10,9 @@ Operator	    Description	Example	        Same as	Result	Decimal
 ^	XOR	        x = 5 ^ 1	0101 ^ 0001	    0100	 4
 <<	Left shift	x = 5 << 1	0101 << 1	    1010	 10
 >>	Right shift	x = 5 >> 1	0101 >> 1	    0010	  2
-*/
-/*
+-------------------------------------------------------------------------
+
+
 operator	    Usage	Description
 Bitwise AND	    a & b	Returns a 1 in each bit position for which the corresponding bits of both operands are 1s.
 Bitwise OR	    a | b	Returns a 1 in each bit position for which the corresponding bits of either or both operands are 1s.
@@ -20,16 +23,11 @@ Sign-propagating
 right shift	    a >> b	Shifts a in binary representation b (< 32) bits to the right, discarding bits shifted off.
 Zero-fill
 right shift	    a >>> b  	Shifts a in binary representation b (< 32) bits to the right, discarding bits shifted off, and shifting in 0s from the left.
-*/
 
-/*
 
 Bitwise shifting any number x to the left by y bits yields x * 2 ** y.
 So e.g.: 9 << 3 translates to: 9 * (2 ** 3) = 9 * (8) = 72.
-
-*/
-
-/*
+ 
 (0 & 0) === 0     // 0 x 0 = 0
 (0 & 1) === 0     // 0 x 1 = 0
 (1 & 0) === 0     // 1 x 0 = 0
