45858d6008647e5c892278f4823b3e226ca48d52
xing wang <uwspstar@gmail.com>
2020-07-08 21:31:04 -0500

    searchMatchStr

--- Data-Structure/JS/recursion/recursion.js
+++ Data-Structure/JS/recursion/recursion.js
@@ -1,5 +1,6 @@
 // https://www.udemy.com/course/coding-interview-bootcamp-algorithms-and-data-structure/learn/lecture/8547214#overview
 
+// 1*2*3*4*...
 function factorial(n) {
     if (n === 1) return 1;
     return n * factorial(n - 1);
