2e45d27ffd74e2b7607abc706955ea8b51cd142a
xing wang <uwspstar@gmail.com>
2020-07-10 21:24:49 -0500

    linked list

---
+++ Data-Structure/JS/Linked List/a-single-linked-list.js
@@ -0,0 +1,31 @@
+// typic
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+// LinkedList only has head 
+class SingleLinkedList {
+    constructor() {
+        this.head = head;
+    }
+
+    size() {
+        let count = 0
+        while (this.head) {
+            count++;
+            this.head = this.head.next;
+        }
+        return count;
+    }
+
+    insertFirst() {
+
+    }
+    add(node) {
+
+    }
+
+}
