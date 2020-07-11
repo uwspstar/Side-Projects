dedca296d66121dbc801042638ea08bbcd0a2fa2
xing wang <uwspstar@gmail.com>
2020-07-11 06:10:47 -0500

    linked list

--- Data-Structure/JS/Linked List/a-singly-linked-list.js
+++ Data-Structure/JS/Linked List/a-singly-linked-list.js
@@ -13,6 +13,12 @@ class SinglyLinkedList {
     constructor(val) {
         this.head = new Node(val);
     }
+    prepend(val) {
+        const node = new Node(val);
+        const current  =  this.head;
+        node.next = current;
+        this.head = node;
+    }
     append(val) {
         const node = new Node(val);
         if (this.head === null) this.head = node;
@@ -29,4 +35,5 @@ class SinglyLinkedList {
 let linkedList = new SinglyLinkedList(5);
 linkedList.append(10);
 linkedList.append(20);
+linkedList.prepend(1);
 console.log(linkedList);
