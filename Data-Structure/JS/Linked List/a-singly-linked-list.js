60d2072b97b3751117c617a79d3a18009f366f3e
xing wang <uwspstar@gmail.com>
2020-07-11 06:03:39 -0500

    linked list

--- Data-Structure/JS/Linked List/a-singly-linked-list.js
+++ Data-Structure/JS/Linked List/a-singly-linked-list.js
@@ -2,30 +2,31 @@
 class Node {
     constructor(val) {
         this.val = val;
-        this.next = null;
+        this.next = null
     }
 }
 
-// LinkedList only has head 
-class SingleLinkedList {
-    constructor() {
-        this.head = head;
-    }
+// let current = this.head; // cannot use "const current = this.head;"
+// compar with Array
 
-    size() {
-        let count = 0
-        while (this.head) {
-            count++;
-            this.head = this.head.next;
+class SinglyLinkedList {
+    constructor(val) {
+        this.head = new Node(val);
+    }
+    append(val) {
+        const node = new Node(val);
+        if (this.head === null) this.head = node;
+        else {
+            let current = this.head; // cannot use "const current = this.head;"
+            while (current.next !== null) {
+                current = current.next;
+            }
+            current.next = node;
         }
-        return count;
     }
-
-    insertFirst() {
-
-    }
-    add(node) {
-
-    }
-
 }
+
+let linkedList = new SinglyLinkedList(5);
+linkedList.append(10);
+linkedList.append(20);
+console.log(linkedList);
--- Data-Structure/JS/Linked List/test.js
+++ Data-Structure/JS/Linked List/test.js
@@ -10,18 +10,19 @@ class SinglyLinkedList {
         this.head = new Node(val);
     }
     append(val) {
-        const current = this.head; 
-        
-        while (current.next!== null) { 
-            current = current.next;
+        const node = new Node(val);
+        if (this.head === null) this.head = node;
+        else {
+            let current = this.head; // cannot use "const current = this.head;"
+            while (current.next !== null) {
+                current = current.next;
+            }
+            current.next = node;
         }
-        current.next = new Node(val);
-
-        console.log('current = ', current, 'current.next=',current.next);
     }
 }
 
 let linkedList = new SinglyLinkedList(5);
 linkedList.append(10);
-// linkedList.append(20);
+linkedList.append(20);
 console.log(linkedList);
