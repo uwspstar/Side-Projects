f1f1f0aa2f757c34be6b6cf9150f50d96ffe2b91
xing wang <uwspstar@gmail.com>
2020-07-12 08:07:13 -0500

    sort

--- Data-Structure/JS/tips/js-Deep-and-Shallow-Copy.js
+++ Data-Structure/JS/tips/js-Deep-and-Shallow-Copy.js
@@ -1,7 +1,7 @@
 // https://we-are.bookmyshow.com/understanding-deep-and-shallow-copy-in-javascript-13438bad941c
 
 /*
-Shallow Copy: It makes a copy of the reference to X into Y. Think about it as a copy of X’s Address. So, the addresses of X and Y will be the same i.e. they will be pointing to the same memory location.
+Shallow Copy: (copy memory address) It makes a copy of the reference to X into Y. Think about it as a copy of X’s Address. So, the addresses of X and Y will be the same i.e. they will be pointing to the same memory location.
 
-Deep copy: It makes a copy of all the members of X, allocates different memory location for Y and then assigns the copied members to Y to achieve deep copy. In this way, if X vanishes Y is still valid in the memory.
+Deep copy (cloning): (copy value) It makes a copy of all the members of X, allocates different memory location for Y and then assigns the copied members to Y to achieve deep copy. In this way, if X vanishes Y is still valid in the memory. The correct term to use would be cloning, where you know that they both are totally the same, but yet different (i.e. stored as two different locations in the memory space).
 */
