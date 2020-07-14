94081dc5e3ad09077408278962cbb111e0d47e63
xing wang <uwspstar@gmail.com>
2020-07-14 15:18:45 -0500

    daily

--- Data-Structure/test.py
+++ Data-Structure/test.py
@@ -16,3 +16,4 @@ def witness(nums):
 print(witness([3, 6, 3, 4, 1]))  # 3 = [6, 4, 1]
 print(witness([3]))  # 1 = [3]
 print(witness([3, 7, 4, 5, 6, 2]))  # 3 = [7, 6, 2]
+print(witness([3, 3, 3, 3, 1]))  # 2 = [3, 1]
