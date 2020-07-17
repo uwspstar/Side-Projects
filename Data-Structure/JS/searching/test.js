1d2fe6b40a55e79bf4815fe4b03686809acb9bb7
xing wang <uwspstar@gmail.com>
2020-07-17 14:58:47 -0500

    daily sort

--- Data-Structure/JS/searching/test.js
+++ Data-Structure/JS/searching/test.js
@@ -1,7 +1,21 @@
+
+// ordered array
+const binarySearchRecursive = function (arr, num, start = 0, end = arr.length - 1) {
+    if (arr.length === 0) return -1;
+    if (start <= end) {
+        let mid = (start + end) >> 1;
+        if (arr[mid] === num) return 1;
+        arr[mid] > num ? binarySearchRecursive(arr, num, mid + 1, end)
+            : binarySearchRecursive(arr, num, start, mid - 1);
+    } else return -1;
+}
+
+console.table(binarySearchRecursive([1, 2, 3, 4, 5], 3));
+/*
 console.log(5 >> 1) // 5 / 2 ** 1
 console.log(Math.floor(5 / 2)) // 5 / 2 ** 1
 console.log(4 / 2) // 5 / 2 ** 1
-/*
+
 const searchMatchStr = function (str, word) {
     if (str.length < word.length) return -1;
     let count = 0;
