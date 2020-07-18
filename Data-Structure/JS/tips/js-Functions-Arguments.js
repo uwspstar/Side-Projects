4e0e33b2efc8a62c2c26d0c69930d3e9a0a2d948
xing wang <uwspstar@gmail.com>
2020-07-17 19:45:23 -0500

    search

--- Data-Structure/JS/tips/js-Functions-Arguments.js
+++ Data-Structure/JS/tips/js-Functions-Arguments.js
@@ -2,14 +2,10 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
 
 function func1(a, b, c) {
-    console.log(arguments[0]);
-    // expected output: 1
 
-    console.log(arguments[1]);
-    // expected output: 2
-
-    console.log(arguments[2]);
-    // expected output: 3
+    console.log(arguments[0]); // expected output: 1
+    console.log(arguments[1]); // expected output: 2
+    console.log(arguments[2]); // expected output: 3
 }
 
 func1(1, 2, 3);
