47cb03c26f59ac51acfd49cf68f962ce2ea126fc
xing wang <uwspstar@gmail.com>
2020-07-18 05:05:18 -0500

    top 500

--- Data-Structure/JS/Top50/00007-is-Palindrome.js
+++ Data-Structure/JS/Top50/00007-is-Palindrome.js
@@ -1,10 +1,26 @@
 // 1: recursive 
+
+const isPalindromeRecursive = function (arr) {
+    if (arr.length < 2) return true;
+    return arr[0] === arr[arr.length - 1] ?
+        isPalindromeRecursive(arr.slice(1, -1)) : false;
+        // cannot use arr.shift().pop() 
+        // arr.shift() is one item
+        // arr.pop() is one item
+        // A negative index can be used, indicating an offset from the end of the sequence. slice(1,-1) extracts the second element through the second-to-last element in the sequence. 
+        // slice(1,-1) remove last one and first one
+}
+
+console.log(isPalindromeRecursive('tacocat'))
+console.log(isPalindromeRecursive('amanaplanacanalpanama'))
+console.log(isPalindromeRecursive('amanaplanacanalpandemonium'))
+
 const isPalindromeRecursive = function (str) {
-    if (str.length === 1) return true
-    if (str.length === 2) return str[0] === str[1]
+    if (str.length === 1) return true;
+    if (str.length === 2) return str[0] === str[1];
     // str.slice(beginIndex[, endIndex])
-    if (str.slice(0, 1) === str.slice(-1)) return isPalindromeRecursive(str.slice(1, -1))
-    return false
+    if (str.slice(0, 1) === str.slice(-1)) return isPalindromeRecursive(str.slice(1, -1));
+    return false;
 }
 
 console.log(isPalindromeRecursive('tacocat'))
