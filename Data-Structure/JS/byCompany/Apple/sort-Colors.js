3e0ced39bfd8f438c819d6e0111efffa5122ba3f
xing wang <uwspstar@gmail.com>
2020-07-19 14:03:31 -0500

    sort

--- Data-Structure/JS/byCompany/Apple/sort-Colors.js
+++ Data-Structure/JS/byCompany/Apple/sort-Colors.js
@@ -1,4 +1,6 @@
 /*
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators
+
 Given an array with n objects colored red, white or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white and blue.
 
 Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
@@ -26,3 +28,67 @@ print(nums) # [0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 2]
 //solution 2 use map to save count of 0, 1 and , loop 3 times add to result arr
 //solution 3 use two pointers, one is for arr[i] = 0 (start++), one for arr[i] = 2 (end --); for arr[i] = 1, not changes
 
+
+
+
+
+//O(4N) =  O(N)
+const sortColor2 = function (arr) {
+  if (arr.length < 2) return arr;
+  let map = new Map();
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    map.has(arr[i]) ? map.set(arr[i], map.get(arr[i]) + 1) : map.set(arr[i], 0);
+  }
+  let n0 = map.get(0);
+  while (n0 > 0) {
+    result.push(0);
+    n0--;
+  }
+  let n1 = map.get(1);
+  while (n1 > 0) {
+    result.push(1);
+    n1--;
+  }
+  let n2 = map.get(2);
+  while (n2 > 0) {
+    result.push(2);
+    n2--;
+  }
+  return result;
+
+}
+
+//O(N), SPACE 
+const sortColor1 = function (arr) {
+  if (arr.length < 2) return arr;
+  let arr0 = [];
+  let arr1 = [];
+  let arr2 = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      arr0.push(arr[i])
+    } else if (arr[i] === 1) {
+      arr1.push(arr[i])
+    } if (arr[i] === 2) {
+      arr2.push(arr[i])
+    }
+  }
+  return [].concat(arr0, arr1, arr2);
+}
+const sortColor1 = function (arr) {
+  if (arr.length < 2) return arr;
+  let arr0 = [];
+  let arr1 = [];
+  let arr2 = [];
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] === 0 ? arr0.push(arr[i]) :
+      arr[i] === 1 ? arr1.push(arr[i]) :
+        arr[i] === 2 ? arr2.push(arr[i]) : null;
+  }
+  return [].concat(arr0, arr1, arr2);
+}
+let arr = [0, 1, 2, 2, 1, 1, 2, 2, 0, 0, 0, 0, 2, 1]
+console.log(sortColor(arr));
+let arr = [0, 1, 2, 2, 1, 1, 2, 2, 0, 0, 0, 0, 2, 1]
+console.log(sortColor(arr));
--- Data-Structure/JS/byCompany/test.js
+++ Data-Structure/JS/byCompany/test.js
@@ -1,18 +1,18 @@
 const sortColor = function (arr) {
     if (arr.length < 2) return arr;
-    let arr0 = [];
-    let arr1 = [];
-    let arr2 = [];
-    for (let i = 0; i < arr.length; i+=) {
-        if (arr[i] === 0) {
-            arr0.push(arr[i])
-        } else if (arr[i] === 1) {
-            arr1.push(arr[i])
-        } if (arr[i] === 2) {
-            arr2.push(arr[i])
+    let start = 0;
+    let end = arr.length - 1;
+    let i = 0
+    while (start < end) {
+        if(arr[start] === 0) {
+            start++
+        } else if (arr[start] === 1) {
+            if (arr[end] === 2) {
+                end--;
+            }
         }
     }
-    return [].concat(arr0, arr1, arr2);
+
 }
 let arr = [0, 1, 2, 2, 1, 1, 2, 2, 0, 0, 0, 0, 2, 1]
 console.log(sortColor(arr));
