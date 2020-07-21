d0b64009b2b7c403fe2c0e3086316e79540ccbb6
xing wang <uwspstar@gmail.com>
2020-07-20 21:37:31 -0500

    daily

--- Data-Structure/JS/byCompany/Microsoft/longest-increasing-subsequence.js
+++ Data-Structure/JS/byCompany/Microsoft/longest-increasing-subsequence.js
@@ -10,3 +10,22 @@ the longest increasing subsequence is [0, 2, 6, 9 , 11, 15].
 // 2: slide window
 
 // 3: dynamic programming
+/*
+// time complexity to O(N2). The space complexity is O(N)
+
+def longest_increasing_subsequence(arr):
+  if not arr:
+    return 0
+  cache = [1] * len(arr)
+  for i in range(1, len(arr)):
+    for j in range(i):
+      if arr[i] > arr[j]:
+        cache[i] = max(cache[i], cache[j] + 1)
+  return max(cache)
+
+
+print longest_increasing_subsequence([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15])
+# 6
+
+
+*/
