226221dd60bf4b97d73162748aca450ccfa5763a
xing wang <uwspstar@gmail.com>
2020-07-22 08:23:45 -0500

    daily

--- Data-Structure/JS/2. Multiple Pointers/sum-zero-1.js
+++ Data-Structure/JS/2. Multiple Pointers/sum-zero-1.js
@@ -1,11 +1,12 @@
-// find first pair of nums sum as zero for a sorted array
-// same as two sum or average pair
+// find first pair of nums sum as zero for a "sorted array"
+// check binary search concept;
+// same as average pair, also check two sum
 //
 const sumZero = function (arr) {
     if (arr.length < 2) return undefined; //at least a pair
 
-    let start = 0
-    let end = arr.length - 1
+    let start = 0;
+    let end = arr.length - 1;
 
     while (start < end) {
 
@@ -14,8 +15,10 @@ const sumZero = function (arr) {
         arr[start] + arr[end] > 0 ? end-- : start++
     }
 
-    return undefined
+    return undefined;
 }
+
+
 console.log(sumZero([-5, -4, -3, -2, 1, 0, -1, 3, 4]))
 console.log(sumZero([-5, -4, -3, -2, 1, 0, -1, 2, 3]))
 console.log(sumZero([-5, -2, -1, 0, 3, 4]))
