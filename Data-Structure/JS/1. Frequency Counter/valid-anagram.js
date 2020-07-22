77baf0da64e59f4f93abf1603b0b8ebe2c4a0caf
xing wang <uwspstar@gmail.com>
2020-07-21 19:15:55 -0500

    daily

--- Data-Structure/JS/1. Frequency Counter/valid-anagram.js
+++ Data-Structure/JS/1. Frequency Counter/valid-anagram.js
@@ -1,22 +1,29 @@
-const validAnagram = function (str1, str2) {
-    if (str1.length !== str2.length) return false
+/*
+2 str have same characters, order does not matter;
+*/
 
-    let map1 = new Map()
-    let map2 = new Map()
+const validAnagram = function (str1, str2) {
+    if (str1.length !== str2.length) return false;
+
+    let map1 = new Map();
+    let map2 = new Map();
 
     for (let i = 0; i < str1.length; i++) {
-        map1.has(str1[i]) ? map1.set(str1[i], map1.get(str1[i]) + 1) : map1.set(str1[i], 1)
-        map2.has(str2[i]) ? map2.set(str2[i], map2.get(str2[i]) + 1) : map2.set(str2[i], 1)
+        let k1 = str1[i];
+        let k2 = str2[i];
+
+        map1.has(k1) ? map1.set(k1, map1.get(k1) + 1) : map1.set(k1, 1);
+        map2.has(k2) ? map2.set(k2, map2.get(k2) + 1) : map2.set(k2, 1);
     }
 
     for (let key of map1.keys()) {
-        if (map2.get(key) !== map1.get(key)) return false
+        if (map2.get(key) !== map1.get(key)) return false;
     }
 
-    return true
+    return true;
 
 }
 
-console.log(validAnagram("abcb", "dbca")) // false
-console.log(validAnagram("abcd", "dbca")) // true
-console.log(validAnagram("mymydog", "mygodym")) // true
+console.log(validAnagram("abcb", "dbca")); // false
+console.log(validAnagram("abcd", "dbca")); // true
+console.log(validAnagram("mymydog", "mygodym")); // true
--- Data-Structure/JS/2. Multiple Pointers/average-Pair.js
+++ Data-Structure/JS/2. Multiple Pointers/average-Pair.js
@@ -1,11 +1,13 @@
 /*
 averagePair
-Write a function called averagePair. Given a sorted array of integers and a target average, determine if there is a pair of values in the array where the average of the pair equals the target average. There may be more than one pair that matches the average target.
+Write a function called averagePair. Given a "sorted array" of integers and a target average, determine if there is a pair of values in the array where the average of the pair equals the target average. 
+There may be more than one pair that matches the average target.
 
 Bonus Constraints: Time: O(N) Space: O(1)
 
 check "two sum" example, different it is sorted array
 check "missing number", sorted array
+for sorted arr, start point = 0, end point = arr.length - 1
 */
 
 const averagePair = function (arr, num) {
