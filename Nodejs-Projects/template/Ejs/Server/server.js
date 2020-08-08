b852d0ee7963f76d19cecce23912b4e5d6d80181
xing wang <uwspstar@gmail.com>
2020-08-08 11:51:27 -0500

    ejs

---
+++ Nodejs-Projects/template/Ejs/Server/server.js
@@ -0,0 +1,33 @@
+var express = require('./node_modules/express'); // class
+var app = express();
+var path = require('path');
+var port = process.env.PORT || 3000;
+
+// set up the view engine and the views directory
+app.set('views', path.join(__dirname, '/client/views'));
+app.set('view engine', 'ejs');
+
+// set up public folder for static files
+app.use(express.static('./public'));
+
+
+// middleware, can do login logic here
+app.use('/', function (req, res, next) {
+    console.log('Request Url:' + req.url);
+    next(); // middleware, can do login logic here
+});
+
+//  ‘/’ route to render the views/index.ejs page
+app.get('/', function (req, res) {
+    res.render('views/index');
+});
+
+app.get('/person/:id', function (req, res) {
+    res.render('person', { ID: req.params.id });
+});
+
+app.get('/api', function (req, res) {
+    res.json({ firstname: 'John', lastname: 'Doe' });
+});
+
+app.listen(port);
--- Nodejs-Projects/template/Ejs/readme.md
+++ Nodejs-Projects/template/Ejs/readme.md
@@ -1,4 +1,5 @@
 ### folder structure
+```
 $ touch readme.md
 $ touch .env
 $ touch .gitignore
@@ -11,4 +12,17 @@ $ mkdir client/views/template
 $ touch client/views/template/head.ejs
 $ touch client/views/template/foot.ejs
 $ touch client/views/template/nav.ejs
+```
+### nodejs init
+```
+$ npm init --y
+$ npm install -D dotenv nodemon
+$ npm install express body-parser
+$ npm install ejs
+```
+### .gitignore
+```
+.env
+node_modules/*
+```
 
