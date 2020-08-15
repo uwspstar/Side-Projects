a55dd00e01aa0dbab37a98592cd7147cfcd9dad5
xing wang <uwspstar@gmail.com>
2020-08-15 05:59:51 -0500

    basic

--- JavaScript/tips/test.js
+++ JavaScript/tips/test.js
@@ -6,6 +6,7 @@ function test(n) {
 }
 //test(3);
 console.log('Number:------------------------------')
+
 console.log('0.1 + 0.2 === 0.3 //', 0.1 + 0.2 === 0.3) // false
 console.log('(0.3 - 0.2) === (0.2 - 0.1) //', (0.3 - 0.2) === (0.2 - 0.1)); // false
 console.log('Math.pow(2, 1024) //', Math.pow(2, 1024)) // Infinity
@@ -13,6 +14,11 @@ console.log('Math.pow(2, 1024) //', Math.pow(2, 1024)) // Infinity
 console.log('Number.MAX_VALUE //', Number.MAX_VALUE)// 1.7976931348623157e+308
 console.log('Number.MIN_VALUE //', Number.MIN_VALUE)// 5e-324
 
+console.log('JavaScript 内部实际上存在2个0：一个是+0，一个是-0')
+console.log('0 / 0 //', 0 / 0) // NaN
+console.log('1 / 0 //', 1 / 0) // Infinity
+
+console.log('switch: ------------------------------')
 function switchTest(x) {
     switch (x) {
         case 1:
@@ -26,7 +32,6 @@ function switchTest(x) {
             console.log('x 等于其他值');
     }
 }
-console.log('switch: ------------------------------')
 console.log(switchTest(1));
 
 console.log('typeof: ------------------------------')
@@ -35,3 +40,5 @@ console.log('typeof window //', typeof window); // object
 console.log('typeof {} //', typeof {}); // object
 console.log('typeof [] //', typeof []); // object
 console.log('typeof null //', typeof null); // object
+
+
