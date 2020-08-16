d4caf6f46c109b4c90fba48fcc675e4b20f5a660
xing wang <uwspstar@gmail.com>
2020-08-16 10:56:25 -0500

    daily

--- JavaScript/tips/IIFE.js
+++ JavaScript/tips/IIFE.js
@@ -1,5 +1,16 @@
 // https://developer.mozilla.org/en-US/docs/Glossary/IIFE
 
+// 1 .
 (function () {
-    statements
+    console.log('I can run as IIFE...')
 })();
+
+/* 
+
+why this follow cannot run as IIFE ?
+
+function foo() { }()
+
+*/
+// 2.
+!function foo() { console.log('I can run ...')}()
