531c6886407b80b6bdb309e1f534944ed9868c98
xing wang <uwspstar@gmail.com>
2020-08-18 15:43:25 -0500

    blog

--- Nodejs-Projects/Blog-Step-by-Step/src/router/user.js
+++ Nodejs-Projects/Blog-Step-by-Step/src/router/user.js
@@ -11,9 +11,22 @@ const handleUserRouter = (req, res) => {
         //return result ? new SuccessModel() : new ErrorModel('Failure to login');
         const { username, password } = req.body;
         const result = checkLogin(username, password);
-        return result.then(data => data.username ? new SuccessModel() : new ErrorModel('Failure to login'));
-
+        return result.then(data => {
+            if (data.username) {
+                // Server side side cookie
+                // path=/ means for root path
+                res.setHeader('Set-Cookie', `username=${data.username}; path=/`)
+                return new SuccessModel()
+            }
+            return new ErrorModel('Failure to login'));
     }
+}
+
+if (method === 'POST' && req.path === '/api/user/login-test') {
+    return req.cookie.username
+        ? Promise.resolve(new SuccessModel())
+        : Promise.reject(new ErrorModel('Failure to login'));
+}
 }
 
 module.exports = handleUserRouter;
