d2ba940e80faf842996ce9c7455ad91fe8581d1c
xing wang <uwspstar@gmail.com>
2020-08-18 15:54:56 -0500

    blog

--- Nodejs-Projects/Blog-Step-by-Step/src/router/user.js
+++ Nodejs-Projects/Blog-Step-by-Step/src/router/user.js
@@ -16,13 +16,15 @@ const handleUserRouter = (req, res) => {
             if (data.username) {
                 // Server side side cookie
                 // path=/ means for root path
-                res.setHeader('Set-Cookie', `username=${data.username}; path=/`)
+                // httpOnly only allow server side modify cookie, not client side (js on browser document.cookie="username=abc")
+                res.setHeader('Set-Cookie', `username=${data.username}; path=/; httpOnly`)
                 return new SuccessModel()
             }
             return new ErrorModel('Failure to login')
         });
     }
 
+    // verify success login
     if (method === 'POST' && req.path === '/api/user/login-test') {
         return req.cookie.username
             ? Promise.resolve(new SuccessModel(req.cookie.username))
