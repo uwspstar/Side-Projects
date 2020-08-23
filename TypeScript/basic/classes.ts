7a6bde0878aafaf5cc36a0557e435b8950a0e692
xing wang <uwspstar@gmail.com>
2020-08-22 20:23:15 -0500

    typescript

---
+++ TypeScript/basic/classes.ts
@@ -0,0 +1,24 @@
+class Vehicle {
+  drive(): void {
+    console.log('go go go go');
+  }
+  honk(): void {
+    console.log('hhhh kkkkk');
+  }
+}
+
+class Car extends Vehicle {
+  drive(): void {
+    console.log('wo oooo');
+  }
+}
+
+/*
+const vehicle = new Vehicle();
+vehicle.drive();
+vehicle.honk();
+*/
+
+const car = new Car();
+car.drive();
+car.honk();
