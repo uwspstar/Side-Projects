73dac68502a4f994d14a59d2100904f750f7f815
xing wang <uwspstar@gmail.com>
2020-09-02 21:58:30 -0500

    blog

--- Nodejs-Projects/Blog-Step-by-Step/src/config/db.js
+++ Nodejs-Projects/Blog-Step-by-Step/src/config/db.js
@@ -2,6 +2,7 @@ const env = process.env.NODE_ENV;
 // for production, do not use nodemon
 
 let MYSQL_CONF = {};
+let REDIS_CONF = {}
 
 switch (env) {
     case 'production':
@@ -11,6 +12,10 @@ switch (env) {
             password: '12345678',
             port: '3306',
             database: 'myblog'
+        }
+        REDIS_CONF = {
+            port: 6379,
+            host: '127.0.0.1'
         }
         break;
 
@@ -22,9 +27,14 @@ switch (env) {
             port: '3306',
             database: 'myblog'
         }
+        REDIS_CONF = {
+            port: 6379,
+            host: '127.0.0.1'
+        }
         break;
 }
 
 module.exports = {
-    MYSQL_CONF
+    MYSQL_CONF,
+    REDIS_CONF
 }
---
+++ Nodejs-Projects/Blog-Step-by-Step/src/db/redis.js
@@ -0,0 +1,44 @@
+const redis = require('redis');
+const { REDIS_CONF } = require('../config/db');
+
+// build client : only connect once, so we do not use quit()
+// port:6379 host:127.0.01
+const redisClient = redis.createClient(REDIS_CONF.port, REDIS_CONF.host);
+redisClient.on('error', err => {
+    console.error(err);
+})
+
+function set(key, value) {
+    if (typeof value === 'object') {
+        value = JSON.stringify(value);
+    }
+    redisClient.set(key, value, redis.print);
+}
+
+function get(key) {
+    const promise = new Promise((resolve, reject) => {
+        redisClient.get(key, (err, val) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            if (val === null) {
+                resolve(null);
+                return;
+            }
+            try {
+                resolve(JSON.parse(val));
+            } catch (ex) {
+                resolve(val);
+            }
+        })
+    });
+    return promise;
+
+}
+
+module.exports = {
+    get,
+    set,
+}
+// test
