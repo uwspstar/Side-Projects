2a952631f4d64508f08bc27041b2e1f457790a3c
xing wang <uwspstar@gmail.com>
2020-09-04 16:11:17 -0500

    blog

--- Nodejs-Projects/Blog-Step-by-Step/src/router/blog.js
+++ Nodejs-Projects/Blog-Step-by-Step/src/router/blog.js
@@ -1,6 +1,90 @@
 const { getList, getDetail, createNewBlog, updateBlog, deleteBlog } = require('../controller/blog');
 const { SuccessModel, ErrorModel } = require('../model/resModel');
 
+
+const checkLogin = (req) => {
+    if (!req.session.username) {
+        Promise.resolve(new ErrorModel('Failure to login'));
+    }
+    /*return req.session.username
+    ? Promise.resolve(new SuccessModel({ login: true }))
+    : Promise.resolve(new ErrorModel({ login: false }))
+    */
+}
+
+const handleBlogRouterAsync = async (req, res) => {
+    const method = req.method;
+    const id = req.query.id || '';
+
+    // get blog list
+    if (method === 'GET' && req.path === '/api/blog/list') {
+        const author = req.query.author || '';
+        const keyword = req.query.keyword || '';
+        //const listData = getList(author, keyword);
+        //return new SuccessModel(listData);
+        const result = getList(author, keyword);
+        const listData = await result;
+        return new SuccessModel(listData);
+    }
+
+    // get blog detail
+    if (method === 'GET' && req.path === '/api/blog/detail') {
+        //const data = getDetail(id);
+        //return new SuccessModel(data);
+        const result = getDetail(id);
+        const data = await result_1;
+        return new SuccessModel(data);
+    }
+
+    // create a new blog
+    if (method === 'POST' && req.path === '/api/blog/new') {
+        //const data = createNewBlog(req.body);
+        //return new SuccessModel(data);
+        const loginResult = checkLogin(req);
+        if (loginResult) {
+            return loginCheck;
+        }
+        //req.body.author = 'xingWang'; //mock data, change it after login
+        req.body.author = req.session.username;
+        const result = createNewBlog(req.body);
+        const data_1 = await result_2;
+        return new SuccessModel(data_1);
+    }
+
+    // Update a new blog
+    if (method === 'POST' && req.path === '/api/blog/update') {
+        //const result = updateBlog(id, req.body);
+        //return result ? new SuccessModel() : new ErrorModel('Failure to Update a blog');
+        const loginResult = checkLogin(req);
+        if (loginResult) {
+            return loginCheck;
+        }
+
+        const result = updateBlog(id, req.body);
+        const val = await result_3;
+        return val ? new SuccessModel(val) : new ErrorModel('Failure to Update a blog');
+
+    }
+
+    // Delete a new blog
+    if (method === 'POST' && req.path === '/api/blog/del') {
+        //const result = deleteBlog(id, req.body);
+        //return result ? new SuccessModel() : new ErrorModel('Failure to Delete a blog');
+
+        const loginResult = checkLogin(req);
+        if (loginResult) {
+            return loginCheck;
+        }
+
+        //const author = 'xingWang';// mock data, update after login
+        const author = req.session.username;
+        const result = deleteBlog(id, author);
+        const val_1 = await result_4;
+        return val_1 ? new SuccessModel(val_1) : new ErrorModel('Failure to Delete a blog');
+    }
+
+}
+
 const handleBlogRouter = (req, res) => {
     const method = req.method;
     const id = req.query.id || '';
@@ -46,10 +130,13 @@ const handleBlogRouter = (req, res) => {
         //const result = deleteBlog(id, req.body);
         //return result ? new SuccessModel() : new ErrorModel('Failure to Delete a blog');
         const author = 'xingWang';// mock data, update after login
-        const result = deleteBlog(id, author); 
+        const result = deleteBlog(id, author);
         return result.then(val => val ? new SuccessModel(val) : new ErrorModel('Failure to Delete a blog'));
     }
 
 }
 
-module.exports = handleBlogRouter;
+module.exports = {
+    handleBlogRouter,
+    handleBlogRouterAsync
+};
--- Nodejs-Projects/Blog-Step-by-Step/src/router/user.js
+++ Nodejs-Projects/Blog-Step-by-Step/src/router/user.js
@@ -9,6 +9,45 @@ const getCookieExpire = () => {
     return d.toGMTString();
 }
 
+const handleUserRouterAsync = async (req, res) => {
+    const method = req.method;
+
+    // user login
+    if (method === 'POST' && req.path === '/api/user/login') {
+        //const { username, password } = req.body;
+        //const result = checkLogin(username, password);
+        //return result ? new SuccessModel() : new ErrorModel('Failure to login');
+        const { username, password } = req.body;
+        const result = checkLogin(username, password);
+
+        const data = await result;
+        if (data.username) {
+            // Server side side cookie
+            // path=/ means for root path
+            // httpOnly only allow server side modify cookie, not client side (js on browser document.cookie="username=abc")
+            // res.setHeader('Set-Cookie', `username=${data.username}; path=/; httpOnly; expire=${getCookieExpire}`)
+            //set session
+            req.session.username = data.username;
+            req.session.realname = data.realname;
+            console.log('req.session: ', req.session);
+            return new SuccessModel();
+        }
+        return new ErrorModel('Failure to login');
+    }
+
+    // verify success login
+    if (method === 'POST' && req.path === '/api/user/login-test') {
+        /*
+        return req.cookie.username
+            ? Promise.resolve(new SuccessModel(req.cookie.username))
+            : Promise.reject(new ErrorModel('Failure to login'));
+        */
+        return req.session.username
+            ? Promise.resolve(new SuccessModel({ session: req.session }))
+            : Promise.reject(new ErrorModel('Failure to login'));
+    }
+}
+
 const handleUserRouter = (req, res) => {
     const method = req.method;
 
@@ -51,4 +90,7 @@ const handleUserRouter = (req, res) => {
     }
 }
 
-module.exports = handleUserRouter;
+module.exports = {
+    handleUserRouter,
+    handleUserRouterAsync
+};
