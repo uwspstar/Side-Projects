580fb815041319829eb9f03a04a279aa4a1f7b61
xing wang <uwspstar@gmail.com>
2020-09-06 23:54:22 -0500

    dotnet

---
+++ CSharp-Projects/vscode/IntroLibary/PersonModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace IntroLibrary
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+    }
+}
