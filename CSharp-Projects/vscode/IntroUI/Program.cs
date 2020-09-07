61d7a5aa258b0d88125ef5d582f167c39ce94325
xing wang <uwspstar@gmail.com>
2020-09-06 23:50:10 -0500

    dotnet

--- CSharp-Projects/vscode/IntroUI/Program.cs
+++ CSharp-Projects/vscode/IntroUI/Program.cs
@@ -7,6 +7,7 @@ namespace IntroUI
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a test");
         }
     }
 }
