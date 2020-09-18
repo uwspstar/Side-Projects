9dd2ce6a85e43ba2c14d10169a8c2905d29090ae
xing wang <uwspstar@gmail.com>
2020-09-17 21:40:32 -0500

    cache

--- CSharp-Projects/Basic-Skills/Generic/Generic.cs
+++ CSharp-Projects/Basic-Skills/Generic/Generic.cs
@@ -6,3 +6,33 @@ public class GenericMethod
         System.Console.WriteLine(t.GetType().Name, t.ToString());
     }
 }
+
+public class GenericClass<T>
+{
+    public T _T;
+}
+public interface IGenericInterface<T>
+{
+    T GetT(T t); // return T
+}
+public delegate void GenericDelegate<T>(T t);
+
+public class CommonClass1<T> : GenericClass<T>
+{
+}
+public class CommonClass2<T> : GenericClass<int>
+{
+    // if CommonClass does have <type>, the GenericClass need has type
+}
+public class CommonClass3 : GenericClass<int>
+{
+    // if CommonClass does have <type>, the GenericClass need has type
+}
+public class CommonClass4<T> : GenericClass<T>, IGenericInterface<T>
+{
+    // if CommonClass does have <type>, the GenericClass need has type
+}
+public class CommonClass4<T> : GenericClass<int>, IGenericInterface<T>
+{
+    // if CommonClass does have <type>, the GenericClass need has type
+}
